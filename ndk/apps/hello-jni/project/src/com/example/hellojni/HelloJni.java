f99249b80b4c2e313f2c530abfb159499a10fe66
Xavier Ducrohet <xav@android.com>
2009-05-07 14:03:22 -0700

    Change the NDK samples package names to be lower case. Also added minSdkVersion=3 in the manifests.

--- ndk/apps/hello-jni/project/src/com/example/hellojni/HelloJni.java
+++ ndk/apps/hello-jni/project/src/com/example/hellojni/HelloJni.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.HelloJni;
+package com.example.hellojni;
 
 import android.app.Activity;
 import android.widget.TextView;
