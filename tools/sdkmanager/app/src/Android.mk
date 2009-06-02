df399393bac20c4d5fcc4c030ff5b76bde50c0be
Raphael <raphael@google.com>
2009-06-02 14:08:46 -0700

    Fix build: Add SWT dependency to SdkManager.

--- tools/sdkmanager/app/src/Android.mk
+++ tools/sdkmanager/app/src/Android.mk
@@ -9,7 +9,11 @@ LOCAL_JAR_MANIFEST := ../etc/manifest.txt
 LOCAL_JAVA_LIBRARIES := \
 	androidprefs \
 	sdklib \
-	sdkuilib
+	sdkuilib \
+	swt \
+	org.eclipse.jface_3.2.0.I20060605-1400 \
+	org.eclipse.equinox.common_3.2.0.v20060603 \
+	org.eclipse.core.commands_3.2.0.I20060605-1400
 LOCAL_MODULE := sdkmanager
 
 include $(BUILD_HOST_JAVA_LIBRARY)
