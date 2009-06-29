a046aadf89720651145e137d41f9f8eb3101d825
David 'Digit' Turner <digit@google.com>
2009-06-29 16:33:30 +0200

    Fix several typos in the NDK build system that caused quite a few bugs:
    - generate thumb code by default (type forced ARM generation)
    
    - make the generation of unoptimized binaries with APP_OPTIM := debug actually work
      (the -O0 -g flags were placed before the -Os -mthumb flags for thumb binaries)
    
    - factor common code from build-executable/shared-library/static-library into build-binary.mk
      and install-binary.mk
    
    - properly add a -L$(SYSROOT)/usr/lib line to make LOCAL_LDLIBS := -lz work as advertized
      (in build-binary.mk)
    
    - add a few missing variables from clear-vars.mk

--- ndk/build/core/build-static-library.mk
+++ ndk/build/core/build-static-library.mk
@@ -14,7 +14,7 @@
 #
 
 # this file is included from Android.mk files to build a target-specific
-# executable program
+# static library
 #
 
 LOCAL_BUILD_SCRIPT := BUILD_STATIC_LIBRARY
@@ -34,24 +34,6 @@ LOCAL_BUILT_MODULE := $(call static-library-path,$(LOCAL_MODULE))
 LOCAL_OBJS_DIR     := $(TARGET_OBJS)/$(LOCAL_MODULE)
 
 include $(BUILD_SYSTEM)/build-binary.mk
-
-static_libraries := $(call map,static-library-path,$(LOCAL_STATIC_LIBRARIES))
-shared_libraries := $(call map,shared-library-path,$(LOCAL_SHARED_LIBRARIES)) \
-                    $(TARGET_PREBUILT_SHARED_LIBRARIES)
-
-$(LOCAL_BUILT_MODULE): $(static_libraries) $(shared_libraries)
-
-LOCAL_LDLIBS := $(_module_libs) $(LOCAL_LDLIBS)
-
-$(LOCAL_BUILT_MODULE): PRIVATE_STATIC_LIBRARIES := $(static_libraries)
-$(LOCAL_BUILT_MODULE): PRIVATE_SHARED_LIBRARIES := $(shared_libraries)
-$(LOCAL_BUILT_MODULE): PRIVATE_OBJECTS          := $(LOCAL_OBJECTS)
-
-$(LOCAL_BUILT_MODULE): PRIVATE_LDFLAGS := $(TARGET_LDFLAGS) $(LOCAL_LDFLAGS)
-$(LOCAL_BUILT_MODULE): PRIVATE_LDLIBS  := $(LOCAL_LDLIBS) $(TARGET_LDLIBS)
-
-$(LOCAL_BUILT_MODULE): PRIVATE_NAME := $(notdir $(LOCAL_BUILT_MODULE))
-$(LOCAL_BUILT_MODULE): PRIVATE_SRC  := $(LOCAL_BUILT_MODULE)
 
 $(LOCAL_BUILT_MODULE): $(LOCAL_OBJECTS)
 	@ mkdir -p $(dir $@)
