b8d30c1a3cc55c9c7ead3bf49701eaba0621745b
Gaurav Mathur <gmathur@google.com>
2009-08-11 17:52:01 -0700

    Checking in frame-work for new Open source site
    	modified:   index.jd
    
    	modified:   index.jd

---
+++ pdk/docs/releases/releases_toc.cs
@@ -0,0 +1,22 @@
+<script type="text/javascript" language="JavaScript">
+<!--
+function nothing() {}
+-->
+</script>
+
+<ul>
+  <li> <h2> Android Releases </h2>
+  <ul>
+    <li><a href="xxx">Android 1.0</a></li>
+    <li><a href="xxx">Android 1.1</a></li>
+    <li><a href="xxx">Android 1.5</a></li>
+  </ul>
+  </li>
+
+</ul>
+
+<script type="text/javascript">
+<!--
+    buildToggleLists();
+//-->
+</script>
