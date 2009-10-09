800158fb99532858d2f67db40ba1bd21120821dc
Gaurav Mathur <gmathur@google.com>
2009-10-08 23:50:24 -0700

    Add new tabs for Compatibility and Get Source to source.android.com framework

---
+++ pdk/docs/compatibility/compatibility_toc.cs
@@ -0,0 +1,31 @@
+<script type="text/javascript" language="JavaScript">
+<!--
+function nothing() {}
+-->
+</script>
+
+<ul>
+  <li> <h2>Program Details </h2>
+  <ul>
+    <li><a href="">Program Overview</a></li>
+    <li><a href="">Getting Started</a></li>
+    <li><a href="">Contact Us</a></li>
+  </ul>
+  </li>
+
+  <li> <h2>Compatibility Tools </h2>
+  <ul>
+    <li><a href="">Compatibility Definition</a></li>
+    <li><a href="">Compatibility Test Suite</a></li>
+    <li><a href="">Compatibility FAQ</a></li>
+    <li><a href="">Downloads</a></li>
+  </ul>
+  </li>
+
+</ul>
+
+<script type="text/javascript">
+<!--
+    buildToggleLists();
+//-->
+</script>
---
+++ pdk/docs/getsource/getsource_toc.cs
@@ -0,0 +1,26 @@
+<script type="text/javascript" language="JavaScript">
+<!--
+function nothing() {}
+-->
+</script>
+
+<ul>
+  <li> <h2>Work with the Code </h2>
+  <ul>
+    <li><a href="">...</a></li>
+  </ul>
+  </li>
+
+  <li> <h2>About the project </h2>
+  <ul>
+    <li><a href="">...</a></li>
+  </ul>
+  </li>
+
+</ul>
+
+<script type="text/javascript">
+<!--
+    buildToggleLists();
+//-->
+</script>
