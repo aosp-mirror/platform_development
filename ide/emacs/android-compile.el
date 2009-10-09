4a5dc7e0c32c57b11c344bb83860a88b0345fd77
Nicolas Catania <niko@google.com>
2009-10-09 09:24:42 -0700

    Minor cleanup, fix documentation.
    Refactored the tests for the makefile existence in a
    separate function.
    Ran checkdoc and fixed errors.

--- ide/emacs/android-compile.el
+++ ide/emacs/android-compile.el
@@ -1,31 +1,49 @@
-;;;
-;;; Copyright (C) 2009 The Android Open Source Project
-;;;
-;;; Licensed under the Apache License, Version 2.0 (the "License");
-;;; you may not use this file except in compliance with the License.
-;;; You may obtain a copy of the License at
-;;;
-;;;      http://www.apache.org/licenses/LICENSE-2.0
-;;;
-;;; Unless required by applicable law or agreed to in writing, software
-;;; distributed under the License is distributed on an "AS IS" BASIS,
-;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-;;; See the License for the specific language governing permissions and
-;;; limitations under the License.
+;;; android-compile.el --- Compile the Android source tree.
+;;
+;; Copyright (C) 2009 The Android Open Source Project
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;      http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
 
-;;; Helper functions to compile Android file within emacs.
-;;; In your .emacs load this file (e.g (require 'android-compile)) then:
-;;;
-;;;   (add-hook 'c++-mode-hook 'android-compile)
-;;;   (add-hook 'java-mode-hook 'android-compile)
-;;; and/or
-;;;   (global-set-key [f9] 'android-compile)
+;;; Commentary:
+;;
+;; Helper functions to compile Android file within emacs.
+;; This module ignores 'build/envsetup.sh' and any enviroment set by the
+;; 'lunch' shell function.
+;; Instead it relies solely on 'buildspec.mk', remember that when you
+;; switch configuration.
+;;
+;; The only interactive function is 'android-compile'.
+;; In your .emacs load this file (e.g (require 'android-compile)) then:
+;;
+;;   (add-hook 'c++-mode-hook 'android-compile)
+;;   (add-hook 'java-mode-hook 'android-compile)
+;; and/or
+;;   (global-set-key [f9] 'android-compile)
+;;
+;;
+;; TODO: Maybe we could cache the result of the compile function in
+;; buffer local vars.
 
-
-;; TODO: Maybe we could cache the result of the compile function in buffer local vars.
+;;; Code:
 
 (require 'compile)
 (require 'android-common)
+
+(defun android-makefile-exists-p (directory)
+  "Return t if an Android makefile exists in DIRECTORY."
+  ; Test for Android.mk first: more likely.
+  (or (file-exists-p (concat directory "Android.mk"))
+      (file-exists-p (concat directory "Makefile"))))
 
 (defun android-find-makefile (topdir)
   "Ascend the current path until an Android makefile is found.
@@ -33,26 +51,30 @@ Makefiles are named Android.mk except in the root directory where
 the file is named Makefile.
 TOPDIR is the root directory of the build.
 Return a list with 2 elements (MAKEFILE_PATH IS_ROOT_MAKEFILE).
+MAKEFILE_PATH is the relative path of the makefile wrt TOPDIR.
 Signal an error if no Makefile was found."
-  ; TODO: Could check that topdir is the start of default-directory.
-  (if (not (> (length topdir) 2))
-      (error "Topdir invalid %s for current dir %s" topdir default-directory))
-  (let ((default-directory default-directory))
-    ; Ascend the path.
+  ;; TODO: Could check that topdir is the start of default-directory.
+  (unless (> (length topdir) 2)
+    (error "Topdir invalid %s for current dir %s" topdir default-directory))
+  (let ((default-directory default-directory)
+        file)
+    ;; Ascend the path.
     (while (and (> (length default-directory) (length topdir))
-                (not (file-exists-p (concat default-directory "Makefile")))
-                (not (file-exists-p (concat default-directory "Android.mk"))))
+                (not (android-makefile-exists-p default-directory)))
       (setq default-directory
             (substring default-directory 0
                        (string-match "[^/]+/$" default-directory))))
-    ; Top dir has a Makefile, otherwise Android.mk files.
-    (if (file-exists-p (concat default-directory "Makefile"))
-        (list (substring (concat default-directory "Makefile")
-                         (length topdir) nil) t)
-      (if (file-exists-p (concat default-directory "Android.mk"))
-          (list (substring (concat default-directory "Android.mk")
-                           (length topdir) nil) nil)
-        (error "Not in a valid android tree.")))))
+
+    (when (not (android-makefile-exists-p default-directory))
+      (error "Not in a valid android tree"))
+
+    (if (string= default-directory topdir)
+        (list "Makefile" t)
+      ;; Remove the root dir at the start of the filename
+      (setq default-directory (substring default-directory (length topdir) nil))
+      (setq file (concat default-directory "Android.mk"))
+      (list file nil))))
+
 
 (defun android-compile ()
   "Elisp equivalent of mm shell function.
@@ -67,8 +89,8 @@ in a compilation."
              (makefile (android-find-makefile topdir))
              (options
               (concat " -j " (number-to-string android-compilation-jobs))))
-        (if (not (file-exists-p (concat topdir "buildspec.mk")))
-            (error "buildspec.mk missing in %s." topdir))
+        (unless (file-exists-p (concat topdir "buildspec.mk"))
+          (error "buildspec.mk missing in %s." topdir))
         (set (make-local-variable 'compile-command)
              (if (cadr makefile)
                  ;; The root Makefile is not invoked using ONE_SHOT_MAKEFILE.
@@ -79,3 +101,5 @@ in a compilation."
             (call-interactively 'compile)))))
 
 (provide 'android-compile)
+
+;;; android-compile.el ends here
