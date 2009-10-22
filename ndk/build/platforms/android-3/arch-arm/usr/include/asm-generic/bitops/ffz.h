b562394bfe9d8df6fcb106eb58c42396d7330b79
android-build SharedAccount <android-build@sekiwake.mtv.corp.google.com>
2009-10-22 10:32:15 -0700

    Part 1 of 2 for merge of ec59a8834bbca256495045a6687a672f66b52df0 to workaround a git issue

--- ndk/build/platforms/android-3/arch-arm/usr/include/asm-generic/bitops/ffz.h
+++ ndk/build/platforms/android-3/arch-arm/usr/include/asm-generic/bitops/ffz.h
@@ -1,1 +1,17 @@
-../../../../../common/include/asm-generic/bitops/ffz.h
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef _ASM_GENERIC_BITOPS_FFZ_H_
+#define _ASM_GENERIC_BITOPS_FFZ_H_
+
+#define ffz(x) __ffs(~(x))
+
+#endif
--- ndk/build/platforms/android-3/arch-arm/usr/include/linux/spinlock_api_smp.h
+++ ndk/build/platforms/android-3/arch-arm/usr/include/linux/spinlock_api_smp.h
@@ -1,1 +1,21 @@
-../../../../common/include/linux/spinlock_api_smp.h
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef __LINUX_SPINLOCK_API_SMP_H
+#define __LINUX_SPINLOCK_API_SMP_H
+
+#ifndef __LINUX_SPINLOCK_H
+#error "please don't include this file directly"
+#endif
+
+#define assert_spin_locked(x) BUG_ON(!spin_is_locked(x))
+
+#endif
