b84cf9e1e378f6cc6642b4ac3f391a85b208a23b
Xavier Ducrohet <xav@android.com>
2009-12-14 22:34:06 -0800

    Clean up the SDK and make it compiled and installed in the SDK system image.
    Change-Id: I6e5f01a1cc45beada70608307f4ac38a4dcdd0b9

--- samples/CubeLiveWallpaper/Android.mk
+++ samples/CubeLiveWallpaper/Android.mk
@@ -17,11 +17,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-# LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_TAGS := samples
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := CubeLiveWallpapers
-LOCAL_CERTIFICATE := shared
+
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
