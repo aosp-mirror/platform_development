9d5338249b7f19a5519add135bb03cc182e31f57
Dan Egnor <egnor@google.com>
2010-03-04 17:29:40 -0800

    More bad behavior: ANR and long-term wedge in system process.
    Use ActivityController (the special monkey hook) to wedge
    ActivityManagerService, useful for triggering system ANRs
    and for setting off the watchdog.
    
    Also add more logging so it's clear what happens when.

--- apps/Development/src/com/android/development/BadBehaviorActivity.java
+++ apps/Development/src/com/android/development/BadBehaviorActivity.java
@@ -17,6 +17,9 @@
 package com.android.development;
 
 import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.IActivityController;
+import android.app.IActivityManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -45,8 +48,9 @@ public class BadBehaviorActivity extends Activity {
     public static class BadReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i(TAG, "in BadReceiver.onReceive() -- about to hang");
+            Log.i(TAG, "in broadcast receiver -- about to hang");
             try { Thread.sleep(20000); } catch (InterruptedException e) { Log.wtf(TAG, e); }
+            Log.i(TAG, "broadcast receiver hang finished -- returning");
         }
     };
 
@@ -58,10 +62,46 @@ public class BadBehaviorActivity extends Activity {
 
         @Override
         public int onStartCommand(Intent intent, int flags, int id) {
-            Log.i(TAG, "in BadService.onStartCommand() -- about to hang");
+            Log.i(TAG, "in service start -- about to hang");
             try { Thread.sleep(30000); } catch (InterruptedException e) { Log.wtf(TAG, e); }
+            Log.i(TAG, "service hang finished -- stopping and returning");
             stopSelf();
             return START_NOT_STICKY;
+        }
+    }
+
+    public static class BadController extends IActivityController.Stub {
+        private int mDelay;
+
+        public BadController(int delay) { mDelay = delay; }
+
+        public boolean activityStarting(Intent intent, String pkg) {
+            try {
+                ActivityManagerNative.getDefault().setActivityController(null);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Can't call IActivityManager.setActivityController", e);
+            }
+
+            if (mDelay > 0) {
+                Log.i(TAG, "in activity controller -- about to hang");
+                try { Thread.sleep(mDelay); } catch (InterruptedException e) { Log.wtf(TAG, e); }
+                Log.i(TAG, "activity controller hang finished -- disabling and returning");
+                mDelay = 0;
+            }
+
+            return true;
+        }
+
+        public boolean activityResuming(String pkg) {
+            return true;
+        }
+
+        public boolean appCrashed(String proc, int pid, String m, String m2, long time, String st) {
+            return true;
+        }
+
+        public int appNotResponding(String proc, int pid, String st) {
+            return 0;
         }
     }
 
@@ -72,6 +112,13 @@ public class BadBehaviorActivity extends Activity {
         if (getIntent().getBooleanExtra("anr", false)) {
             Log.i(TAG, "in ANR activity -- about to hang");
             try { Thread.sleep(20000); } catch (InterruptedException e) { Log.wtf(TAG, e); }
+            Log.i(TAG, "activity hang finished -- finishing");
+            finish();
+            return;
+        }
+
+        if (getIntent().getBooleanExtra("dummy", false)) {
+            Log.i(TAG, "in dummy activity -- finishing");
             finish();
             return;
         }
@@ -127,6 +174,7 @@ public class BadBehaviorActivity extends Activity {
             public void onClick(View v) {
                 Log.i(TAG, "ANR pressed -- about to hang");
                 try { Thread.sleep(20000); } catch (InterruptedException e) { Log.wtf(TAG, e); }
+                Log.i(TAG, "hang finished -- returning");
             }
         });
 
@@ -152,6 +200,36 @@ public class BadBehaviorActivity extends Activity {
             public void onClick(View v) {
                 Log.i(TAG, "ANR service pressed -- about to start");
                 startService(new Intent(BadBehaviorActivity.this, BadService.class));
+            }
+        });
+
+        Button anr_system = (Button) findViewById(R.id.bad_behavior_anr_system);
+        anr_system.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(BadBehaviorActivity.this, BadBehaviorActivity.class);
+                Log.i(TAG, "ANR system pressed -- about to engage");
+                try {
+                    ActivityManagerNative.getDefault().setActivityController(
+                        new BadController(20000));
+                } catch (RemoteException e) {
+                    Log.e(TAG, "Can't call IActivityManager.setActivityController", e);
+                }
+                startActivity(intent.putExtra("dummy", true));
+            }
+        });
+
+        Button wedge_system = (Button) findViewById(R.id.bad_behavior_wedge_system);
+        wedge_system.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(BadBehaviorActivity.this, BadBehaviorActivity.class);
+                Log.i(TAG, "Wedge system pressed -- about to engage");
+                try {
+                    ActivityManagerNative.getDefault().setActivityController(
+                        new BadController(300000));
+                } catch (RemoteException e) {
+                    Log.e(TAG, "Can't call IActivityManager.setActivityController", e);
+                }
+                startActivity(intent.putExtra("dummy", true));
             }
         });
     }
