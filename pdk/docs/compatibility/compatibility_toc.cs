98620ee0ab42c52beeb5f008b734a48c65fbc167
Dan Morrill <morrildl@google.com>
2010-04-13 14:48:25 -0700

    Various updates to add missing content for impending launch.
    Change-Id: I8423b4404c26330fc9391777247a2620effb03cf

--- pdk/docs/compatibility/compatibility_toc.cs
+++ pdk/docs/compatibility/compatibility_toc.cs
@@ -7,12 +7,12 @@ function nothing() {}
 <ul>
   <li><h2>Getting Started</h2><ul>
     <li><a href="<?cs var:toroot ?>compatibility/overview.html">Compatibility Overview</a></li>
-    <li><a href="">Current CDD</a></li>
+    <li><a href="<?cs var:toroot ?>compatibility/android-2.1-cdd.pdf">Current CDD</a></li>
     <li><a href="<?cs var:toroot ?>compatibility/cts-intro.html">CTS Introduction</a></li>
   </ul></li>
 
   <li><h2>More Information</h2><ul>
-    <li><a href="<?cs var:toroot ?>downloads/index.html">Downloads</a></li>
+    <li><a href="<?cs var:toroot ?>compatibility/downloads.html">Downloads</a></li>
     <li><a href="<?cs var:toroot ?>faqs.html#compatibility">FAQs</a></li>
     <li><a href="<?cs var:toroot ?>compatibility/contact-us.html">Contact Us</a></li>
   </ul></li>
