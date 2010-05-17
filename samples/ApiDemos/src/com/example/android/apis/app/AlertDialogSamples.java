b88f42b3618a688ad395b3de14b1e54cefdeff60
Christian Mehlmauer <FireFart@gmail.com>
2010-05-17 20:36:01 +0200

    Removed Calls to deprecated API
    Change-Id: Ia94734637df6aa7b8b08417c49a3485f34f1b94c

--- samples/ApiDemos/src/com/example/android/apis/app/AlertDialogSamples.java
+++ samples/ApiDemos/src/com/example/android/apis/app/AlertDialogSamples.java
@@ -135,13 +135,15 @@ public class AlertDialogSamples extends Activity {
             mProgressDialog.setTitle(R.string.select_dialog);
             mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             mProgressDialog.setMax(MAX_PROGRESS);
-            mProgressDialog.setButton(getText(R.string.alert_dialog_hide), new DialogInterface.OnClickListener() {
+            mProgressDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                    getText(R.string.alert_dialog_hide), new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
 
                     /* User clicked Yes so do some stuff */
                 }
             });
-            mProgressDialog.setButton2(getText(R.string.alert_dialog_cancel), new DialogInterface.OnClickListener() {
+            mProgressDialog.setButton(DialogInterface.BUTTON_NEGATIVE,
+                    getText(R.string.alert_dialog_cancel), new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
 
                     /* User clicked No so do some stuff */
