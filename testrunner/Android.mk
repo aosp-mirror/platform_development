c2b27167ddcbc565a5dae7a80448111e377a0bd6
Guang Zhu <guangzhu@google.com>
2010-06-02 11:18:14 -0700

    fix earlier change for coverage_targets.xml
    due to previous incorrect change to Makefile, test_defs.xml is missing.
    this will add it back in.
    
    Change-Id: I4bd57d24757662477409ba02d3f71122e8e75238

--- testrunner/Android.mk
+++ testrunner/Android.mk
@@ -3,23 +3,23 @@
 #
 
 # where to install the sample files on the device
-# 
+#
 local_target_dir := $(TARGET_OUT_DATA)/testinfo
 LOCAL_PATH := $(call my-dir)
 
 ########################
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := test_defs.xml
 LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := coverage_targets.xml
 LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
 include $(BUILD_PREBUILT)
