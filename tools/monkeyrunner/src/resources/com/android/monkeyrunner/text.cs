2e163ac64983a69718f895e534fa03a87cbd6c7d
Bill Napier <napier@google.com>
2010-07-27 16:25:38 -0700

    Move MonkeyRunner from development to sdk.
    Change-Id: I87ac348a48d64706aee2e66cf833944b15b70dd8

--- tools/monkeyrunner/src/resources/com/android/monkeyrunner/text.cs
+++
@@ -1,9 +0,0 @@
-MonkeyRunner help
-<?cs each:item = help ?>
-<?cs var:item.name ?>
-  <?cs var:item.doc ?>
-
-<?cs if:subcount(item.argument) ?>  Args:<?cs each:arg = item.argument ?>
-    <?cs var:arg.name ?> - <?cs var:arg.doc ?><?cs /each ?>
-<?cs /if ?>  Returns: <?cs var:item.returns ?>
-<?cs /each ?>
