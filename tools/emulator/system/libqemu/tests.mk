e1eea50214580061e18e71e9b1ed56c7e721b751
David 'Digit' Turner <digit@android.com>
2011-03-16 14:19:52 +0100

    emulator: Add libqemu static library to implement qemu pipes.
    See hardware/libhardware/include/hardware/qemu_pipe.h for the API
    implemented by the library. It enables very fast reads/writes between
    the guest system and specific emulator services.
    
    Define BUILD_LIBQEMU_TESTS=true in your environment to build the
    test programs (a simple host ping-pong server, and a benchmark
    guest program).
    
    You can invoke them with:
    
    1/ Testing TCP pipes:
    
       host:  test-libqemu-1 -tcp 8012
       guest: test-libqemu-2 -pipe tcp:8012
    
    Alternatively
    
       guest: su
              test-libqemu-2 -tcp 8012
    
    2/ Testing Unix pipes:
    
       host:  test-libqemu-1 -unix /tmp/libqemu-socket
       guest: test-libqemu-2 -pipe unix:/tmp/libqemu-socket
    
    3/ Testing internal pingpong server (within the emulator)
    
       host:   /* nothing to do */
       guest:  test-libqemu-2
    
    Change-Id: Ib50fc9cbee6b5f4581baca97412d6f69d4f84860

---
+++ tools/emulator/system/libqemu/tests.mk
@@ -0,0 +1,30 @@
+# Build libqemu tests, included from main Android.mk
+
+# The first test program is a simple TCP server that will send back
+# anything it receives from the client.
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := test-libqemu-1
+LOCAL_SRC_FILES := test_host_1.c
+LOCAL_MODULE_TAGS := debug
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := test-libqemu-2
+LOCAL_SRC_FILES := test_host_2.c
+LOCAL_MODULE_TAGS := debug
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := test-libqemu-1
+LOCAL_SRC_FILES := test_guest_1.c test_util.c
+LOCAL_MODULE_TAGS := debug
+LOCAL_STATIC_LIBRARIES := libqemu libcutils
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := test-libqemu-2
+LOCAL_SRC_FILES := test_guest_2.c test_util.c
+LOCAL_MODULE_TAGS := debug
+LOCAL_STATIC_LIBRARIES := libqemu libcutils
+include $(BUILD_EXECUTABLE)
