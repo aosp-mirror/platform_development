a4205ba264bd96ddf44429de7973291ce437249e
Jacky Romano <jr@graphtech.co.il>
2011-03-27 18:06:27 +0200

    Android emulator opengles - unit test gles wrapper
    This adds opengl es implementation driver that selects whether to use
    the native android emulator opengl impelementation or use a remote,
    host side renderer. This driver is used to selectivley run applications using
    the remote renderer. The selection is based on the application/process name and
    is configured using /etc/gles_emul.cfg in the emulator side.
    
    This change also installs /etc/egl.cfg that is used by the Android opengl wrapper
    to select an opengl implementation
    
    Change-Id: I66eda3d48938f8057bf45e6476b7faefaf1130fb

---
+++ tools/emulator/opengl/tests/gles_android_wrapper/Android.mk
@@ -0,0 +1,68 @@
+LOCAL_PATH := $(call my-dir)
+
+##### libGLES_emul.so ###########
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=  \
+        egl.cpp \
+        egl_dispatch.cpp \
+        gles.cpp \
+        gles_dispatch.cpp \
+        ServerConnection.cpp \
+        ThreadInfo.cpp
+
+# add additional depencies to ensure that the generated code that we depend on
+# is generated
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libut_rendercontrol_enc$(TARGET_SHLIB_SUFFIX) \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libGLESv1_enc$(TARGET_SHLIB_SUFFIX)
+
+
+emulatorOpengl := $(LOCAL_PATH)/../..
+
+LOCAL_C_INCLUDES := $(emulatorOpengl)/system/OpenglCodecCommon \
+		$(call intermediates-dir-for, SHARED_LIBRARIES, libut_rendercontrol_enc) \
+		$(call intermediates-dir-for, SHARED_LIBRARIES, libGLESv1_enc) \
+        $(emulatorOpengl)/system/GLESv1_enc \
+        $(emulatorOpengl)/tests/ut_rendercontrol_enc 
+
+
+LOCAL_CFLAGS := -DLOG_TAG=\"eglWrapper\"
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/egl
+LOCAL_MODULE := libGLES_emul
+LOCAL_MODULE_TAGS := debug
+LOCAL_PRELINK_MODULE := false
+
+#LOCAL_LDLIBS := -lpthread -ldl
+LOCAL_SHARED_LIBRARIES := libdl libcutils libGLESv1_enc libut_rendercontrol_enc
+LOCAL_STATIC_LIBRARIES := libOpenglCodecCommon
+
+include $(BUILD_SHARED_LIBRARY)
+
+#### egl.cfg ####
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := egl.cfg
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/egl
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := ETC
+
+include $(BUILD_PREBUILT)
+
+#### gles_emul.cfg ####
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gles_emul.cfg
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := ETC
+
+include $(BUILD_PREBUILT)
+
+
+
+
