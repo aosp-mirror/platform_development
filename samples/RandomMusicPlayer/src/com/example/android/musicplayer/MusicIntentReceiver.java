b30a3010f51cb3631011d84ac948496f044fc7f5
Bruno Oliveira <btco@google.com>
2011-06-07 01:09:21 -0400

    Adds the Random Music Player sample.
    This sample illustrates how to write an application that plays media
    in the background, handles streaming media from the network and
    reacts appropriately to system events like headphone disconnection and
    audio focus changes, and also uses the notification system to
    keep the user informed about what is happening.
    
    This CL is a cherrypick of the old CL 104495 (originally in
    honeycomb-mr1) into honeycomb-mr2. Original CL:
    https://android-git.corp.google.com/g/#change,104495
    
    Change-Id: Ib8b15ee920300adc093a8b52c17bd9c251ac74ca

---
+++ samples/RandomMusicPlayer/src/com/example/android/musicplayer/MusicIntentReceiver.java
@@ -0,0 +1,40 @@
+/*   
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.musicplayer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Receives broadcasted intents. In particular, we are interested in the
+ * android.media.AUDIO_BECOMING_NOISY intent, which is broadcast, for example, when the user
+ * disconnects the headphones. This class works because we are declaring it in a &lt;receiver&gt;
+ * tag in AndroidManifest.xml.
+ */
+public class MusicIntentReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        if (intent.getAction().equals(android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
+            Toast.makeText(ctx, "Headphones disconnected.", Toast.LENGTH_SHORT).show();
+
+            // send an intent to our MusicService to telling it to pause the audio
+            ctx.startService(new Intent(MusicService.ACTION_PAUSE));
+        }
+    }
+}
