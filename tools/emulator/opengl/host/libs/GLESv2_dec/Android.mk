76780669f9867587693563358ccdc903e9cdcbba
Andrew Hsieh <andrewhsieh@google.com>
2012-03-02 00:19:49 -0800

    Added rules to build 64-bit libraries for 64-bit emulator
    All ten libraries can now be built in 64-bit named "lib64*" (*)
    in addition to the original 32-bit form named "lib*".
    
    Also, dlopen "lib64*so" in 64-bit.
    
    (*) eg. In Ubuntu, all can be built with the following command:
      make out/host/linux-x86/lib/lib64OpenglRender.so \
           out/host/linux-x86/lib/lib64EGL_translator.so \
           out/host/linux-x86/lib/lib64GLES_CM_translator.so \
           out/host/linux-x86/lib/lib64GLES_V2_translator.so
    
    Rules to build static libraries lib64log.a, lib64cutils.a and lib64utils.a
    they depend were added in other CLs.
    
    Change-Id: I3afb64de6dda1d55dbd1b4443d2dbc78a683b19f

--- tools/emulator/opengl/host/libs/GLESv2_dec/Android.mk
+++ tools/emulator/opengl/host/libs/GLESv2_dec/Android.mk
@@ -1,5 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
+host_common_debug_CFLAGS :=
+
+#For gl debbuging
+#host_common_debug_CFLAGS += -DCHECK_GL_ERROR
+#host_common_debug_CFLAGS += -DDEBUG_PRINTOUT
+
+
+### host library ##########################################
 $(call emugl-begin-host-static-library,libGLESv2_dec)
 $(call emugl-import, libOpenglCodecCommon libOpenglOsUtils)
 $(call emugl-gen-decoder,$(EMUGL_PATH)/system/GLESv2_enc,gl2)
@@ -8,9 +16,22 @@ $(call emugl-gen-decoder,$(EMUGL_PATH)/system/GLESv2_enc,gl2)
 $(call emugl-export,C_INCLUDES,$(EMUGL_PATH)/system/GLESv2_enc)
 $(call emugl-export,C_INCLUDES,$(LOCAL_PATH))
 
-#For gl debbuging
-#$(call emugl-export,CFLAGS,-DCHECK_GL_ERROR)
-#$(call emugl-export,CFLAGS,-DDEBUG_PRINTOUT)
+$(call emugl-export,CFLAGS,$(host_common_debug_CFLAGS))
+
+LOCAL_SRC_FILES := GL2Decoder.cpp
+
+$(call emugl-end-module)
+
+### host library, 64-bit ####################################
+$(call emugl-begin-host-static-library,lib64GLESv2_dec)
+$(call emugl-import, lib64OpenglCodecCommon lib64OpenglOsUtils)
+$(call emugl-gen-decoder,$(EMUGL_PATH)/system/GLESv2_enc,gl2)
+
+# For gl2_types.h !
+$(call emugl-export,C_INCLUDES,$(EMUGL_PATH)/system/GLESv2_enc)
+$(call emugl-export,C_INCLUDES,$(LOCAL_PATH))
+
+$(call emugl-export,CFLAGS,$(host_common_debug_CFLAGS) -m64)
 
 LOCAL_SRC_FILES := GL2Decoder.cpp
 
--- tools/emulator/opengl/host/libs/libOpenglRender/Android.mk
+++ tools/emulator/opengl/host/libs/libOpenglRender/Android.mk
@@ -1,28 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
-### libOpenglRender #################################################
-$(call emugl-begin-host-shared-library,libOpenglRender)
-
-$(call emugl-import,libGLESv1_dec libGLESv2_dec lib_renderControl_dec libOpenglCodecCommon libOpenglOsUtils)
-
-OS_SRCS:=
+host_OS_SRCS :=
+host_common_LDLIBS :=
 
 ifeq ($(HOST_OS),linux)
-    OS_SRCS = NativeLinuxSubWindow.cpp
-    LOCAL_LDLIBS += -lX11
+    host_OS_SRCS = NativeLinuxSubWindow.cpp
+    host_common_LDLIBS += -lX11
 endif
 
 ifeq ($(HOST_OS),darwin)
-    OS_SRCS = NativeMacSubWindow.m
-    LOCAL_LDLIBS += -Wl,-framework,AppKit
+    host_OS_SRCS = NativeMacSubWindow.m
+    host_common_LDLIBS += -Wl,-framework,AppKit
 endif
 
 ifeq ($(HOST_OS),windows)
-    OS_SRCS = NativeWindowsSubWindow.cpp
+    host_OS_SRCS = NativeWindowsSubWindow.cpp
 endif
 
-LOCAL_SRC_FILES := \
-    $(OS_SRCS)      \
+host_common_SRC_FILES := \
+    $(host_OS_SRCS) \
     render_api.cpp \
     ColorBuffer.cpp \
     EGLDispatch.cpp \
@@ -38,6 +34,20 @@ LOCAL_SRC_FILES := \
     ReadBuffer.cpp \
     RenderServer.cpp
 
+host_common_CFLAGS :=
+
+#For gl debbuging
+#host_common_CFLAGS += -DCHECK_GL_ERROR
+
+
+### host libOpenglRender #################################################
+$(call emugl-begin-host-shared-library,libOpenglRender)
+
+$(call emugl-import,libGLESv1_dec libGLESv2_dec lib_renderControl_dec libOpenglCodecCommon libOpenglOsUtils)
+
+LOCAL_LDLIBS += $(host_common_LDLIBS)
+
+LOCAL_SRC_FILES := $(host_common_SRC_FILES)
 $(call emugl-export,C_INCLUDES,$(EMUGL_PATH)/host/include)
 $(call emugl-export,C_INCLUDES,$(LOCAL_PATH))
 
@@ -46,7 +56,28 @@ LOCAL_C_INCLUDES += $(EMUGL_PATH)/host/libs/Translator/include
 
 LOCAL_STATIC_LIBRARIES += libutils liblog
 
-#For gl debbuging
-#$(call emugl-export,CFLAGS,-DCHECK_GL_ERROR)
+$(call emugl-export,CFLAGS,$(host_common_CFLAGS))
+
+$(call emugl-end-module)
+
+
+### host libOpenglRender, 64-bit #########################################
+$(call emugl-begin-host-shared-library,lib64OpenglRender)
+
+$(call emugl-import,lib64GLESv1_dec lib64GLESv2_dec lib64_renderControl_dec lib64OpenglCodecCommon lib64OpenglOsUtils)
+
+#LOCAL_LDFLAGS += -m64  # adding -m64 here doesn't work, because it somehow appear BEFORE -m32 in command-line.
+LOCAL_LDLIBS += $(host_common_LDLIBS) -m64  # Put -m64 it in LOCAL_LDLIBS instead.
+
+LOCAL_SRC_FILES := $(host_common_SRC_FILES)
+$(call emugl-export,C_INCLUDES,$(EMUGL_PATH)/host/include)
+$(call emugl-export,C_INCLUDES,$(LOCAL_PATH))
+
+# use Translator's egl/gles headers
+LOCAL_C_INCLUDES += $(EMUGL_PATH)/host/libs/Translator/include
+
+LOCAL_STATIC_LIBRARIES += lib64utils lib64log
+
+$(call emugl-export,CFLAGS,$(host_common_CFLAGS) -m64)
 
 $(call emugl-end-module)
