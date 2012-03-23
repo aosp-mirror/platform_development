29539e93c9d8dfe03934458288597a12e5ed3693
Andrew Hsieh <andrewhsieh@google.com>
2012-03-23 16:12:57 +0800

    Fixed to #include correct 32-bit headers
    Corrected for example <asm/io.h> to include io_32.h instead of io_64.h
    (which is missing, BTW).
    
    Copied from the results of commit (same title) in bionic.git:
    126601dd3f5303b50033dcb88945d928aa764aa4
    
    Change-Id: I49547d5253459396bfc85012b1fd263ad5c7e9d0

--- ndk/platforms/android-9/arch-x86/include/asm/bug.h
+++ ndk/platforms/android-9/arch-x86/include/asm/bug.h
@@ -7,10 +7,17 @@
  ***   structures, and macros generated from the original header, and thus,
  ***   contains no copyrightable information.
  ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
  ****************************************************************************
  ****************************************************************************/
 #ifndef _ASM_X86_BUG_H
 #define _ASM_X86_BUG_H
-
 #include <asm-generic/bug.h>
 #endif
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
