b10b6f2baf2090c932ff17758b57c5c23a976c13
Pete Delaney <piet@mips.com>
2012-10-05 00:23:49 -0700

    [MIPS] Update Support for fcntl.
    Change-Id: I743837793a84e6f4dbbc192b29b19a6eae78a5da
    Signed-off-by: Pete Delaney  <piet@mips.com>
    Signed-off-by: Chris Dearman <chris@mips.com>

--- ndk/sources/android/libportable/common/include/fcntl_portable.h
+++ ndk/sources/android/libportable/common/include/fcntl_portable.h
@@ -18,7 +18,7 @@
 #define _FCNTL_PORTABLE_H_
 
 /* Derived from development/ndk/platforms/android-3/arch-arm/include/asm/fcntl.h */
-/* NB x86 does not have these and only uses the generic definitions */
+/* NB x86 does not have these and only uses the generic definitions. */
 #define O_DIRECTORY_PORTABLE    040000
 #define O_NOFOLLOW_PORTABLE     0100000
 #define O_DIRECT_PORTABLE       0200000
@@ -72,6 +72,21 @@
 #define O_NDELAY_PORTABLE   O_NONBLOCK_PORTABLE
 #endif
 
+/*
+ * For use with F_GETLK and F_SETLK
+ */
+struct flock_portable {
+   short l_type;
+   short l_whence;
+   off_t l_start;
+   off_t l_len;
+   pid_t l_pid;
+   __ARCH_FLOCK64_PAD
+};
+
+/*
+ * For use with F_GETLK64 and F_SETLK64
+ */
 struct flock64_portable {
    short l_type;
    short l_whence;
@@ -82,9 +97,10 @@ struct flock64_portable {
    __ARCH_FLOCK64_PAD
 };
 
+#if 0
 /*
-The X86 Version is
-
+ * The X86 Version is
+ */
 struct flock64 {
    short l_type;
    short l_whence;
@@ -93,16 +109,31 @@ struct flock64 {
    pid_t l_pid;
    __ARCH_FLOCK64_PAD
 };
-*/
+#endif /* 0 */
+
+
+#ifndef F_DUPFD_PORTABLE
+#define F_DUPFD_PORTABLE 0
+#define F_GETFD_PORTABLE 1
+#define F_SETFD_PORTABLE 2
+#define F_GETFL_PORTABLE 3
+#define F_SETFL_PORTABLE 4
+#endif
 
 #ifndef F_GETLK_PORTABLE
 #define F_GETLK_PORTABLE 5
 #define F_SETLK_PORTABLE 6
 #define F_SETLKW_PORTABLE 7
 #endif
+
 #ifndef F_SETOWN_PORTABLE
 #define F_SETOWN_PORTABLE 8
 #define F_GETOWN_PORTABLE 9
+#endif
+
+#ifndef F_SETSIG_PORTABLE
+#define F_SETSIG_PORTABLE 10
+#define F_GETSIG_PORTABLE 11
 #endif
 
 #ifndef F_GETLK64_PORTABLE
@@ -110,5 +141,16 @@ struct flock64 {
 #define F_SETLK64_PORTABLE 13
 #define F_SETLKW64_PORTABLE 14
 #endif
+
+/* This constant seems to be the same for all ARCH's */
+#define F_LINUX_SPECIFIC_BASE_PORTABLE 1024
+
+#define F_SETLEASE_PORTABLE             (F_LINUX_SPECIFIC_BASE+0)       /* 1024 */
+#define F_GETLEASE_PORTABLE             (F_LINUX_SPECIFIC_BASE+1)       /* 1025 */
+#define F_NOTIFY_PORTABLE               (F_LINUX_SPECIFIC_BASE+2)       /* 1026 */
+
+/* Currently these are only supported by X86_64 */
+#define F_CANCELLK_PORTABLE             (F_LINUX_SPECIFIC_BASE+5)       /* 1029 */
+#define F_DUPFD_CLOEXEC_PORTABLE        (F_LINUX_SPECIFIC_BASE+6)       /* 1030 */
 
 #endif /* _FCNTL_PORTABLE_H */
