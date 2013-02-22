8227c1490235509cbb51397c15ad17c5bb054507
Dianne Hackborn <hackbod@google.com>
2013-02-22 15:09:41 -0800

    New overscan API demos.
    A new flag on the system UI demo (which needs to be renamed),
    and a new pure overscan demo.
    
    Change-Id: I88a7a309147aaad773bbc00971a01369f4658b61

---
+++ samples/ApiDemos/src/com/example/android/apis/app/OverscanActivity.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.apis.app;
+
+// Need the following import to get access to the app resources, since this
+// class is in a sub-package.
+import android.widget.ImageView;
+import com.example.android.apis.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * <h3>Overscan Activity</h3>
+ * 
+ * <p>This demonstrates the how to write an activity that extends into the
+ * overscan region.</p>
+ */
+public class OverscanActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overscan_activity);
+    }
+}
