3b8f424558c59f2620bac6df511c9b1b1859c799
Tom O'Neill <tomo@google.com>
2013-08-15 13:25:19 -0700

    Settings injector sample activity
    - Tests implementation of b/10287745
    
    Change-Id: I5bf3323be78a16b660eadb96eadc6bf29197ace5

---
+++ apps/SettingInjectorSample/src/com/example/android/injector/MySettingActivity.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.injector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+/**
+ * A minimal "Hello, World!" application.
+ */
+public class MySettingActivity extends Activity {
+    /**
+     * Called with the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the layout for this activity.  You can find it
+        // in res/layout/my_setting_activity.xml
+        View view = getLayoutInflater().inflate(R.layout.my_setting_activity, null);
+        setContentView(view);
+    }
+}
+
