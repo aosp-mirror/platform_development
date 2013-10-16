6570d523c46ab792d4af555d34451e976a51a001
Ben Cheng <bccheng@google.com>
2013-10-16 10:10:42 -0700

    Use updated uapi headers for aarch64.
    Change-Id: I6093b3908f26842524acd5ba845e083b8ad6a888

--- ndk/platforms/android-9/arch-aarch64/include/asm/bitsperlong.h
+++ ndk/platforms/android-9/arch-aarch64/include/asm/bitsperlong.h
@@ -16,4 +16,9 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#include <asm-generic/posix_types.h>
+#ifndef __ASM_BITSPERLONG_H
+#define __ASM_BITSPERLONG_H
+#define __BITS_PER_LONG 64
+#include <asm-generic/bitsperlong.h>
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#endif
--- ndk/platforms/android-9/arch-aarch64/include/asm/siginfo.h
+++ ndk/platforms/android-9/arch-aarch64/include/asm/siginfo.h
@@ -7,11 +7,18 @@
  ***   structures, and macros generated from the original header, and thus,
  ***   contains no copyrightable information.
  ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _ASMARM_SIGINFO_H
-#define _ASMARM_SIGINFO_H
-
+#ifndef __ASM_SIGINFO_H
+#define __ASM_SIGINFO_H
+#define __ARCH_SI_PREAMBLE_SIZE (4 * sizeof(int))
 #include <asm-generic/siginfo.h>
-
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #endif
