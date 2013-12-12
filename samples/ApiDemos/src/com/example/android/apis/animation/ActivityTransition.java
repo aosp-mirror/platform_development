8ad5ffb4618350715cfef346e553b2b0501840eb
George Mount <mount@google.com>
2013-12-12 11:05:15 -0800

    Support changes in Scene Activity Transition API.
    Change-Id: Ia19b7def404f32ea5f68d2fece04dcd5ce615a42

--- samples/ApiDemos/src/com/example/android/apis/animation/ActivityTransition.java
+++ samples/ApiDemos/src/com/example/android/apis/animation/ActivityTransition.java
@@ -17,23 +17,17 @@ package com.example.android.apis.animation;
 
 import com.example.android.apis.R;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
-import android.animation.RectEvaluator;
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.Intent;
-import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.transition.TransitionSet;
-import android.util.Property;
+import android.util.ArrayMap;
+import android.util.Pair;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewGroupOverlay;
 import android.view.Window;
 import android.widget.ImageView;
 
@@ -43,138 +37,50 @@ import java.util.Random;
  *
  */
 public class ActivityTransition extends Activity {
+
     private static final String TAG = "ActivityTransition";
 
-    private static final String KEY_LEFT_ON_SCREEN = "ViewTransitionValues:left:";
-    private static final String KEY_TOP_ON_SCREEN = "ViewTransitionValues:top:";
-    private static final String KEY_WIDTH = "ViewTransitionValues:width:";
-    private static final String KEY_HEIGHT = "ViewTransitionValues:height:";
     private static final String KEY_ID = "ViewTransitionValues:id";
 
     private Random mRandom = new Random();
-    private boolean mComeBack;
-    private Fall mFall;
+
+    private ImageView mHero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-        setEarlyBackgroundTransition(false);
+        getWindow().setTriggerEarlyEnterTransition(true);
         getWindow().setBackgroundDrawable(new ColorDrawable(randomColor()));
         setContentView(R.layout.image_block);
-        View hero = getHero();
-        if (hero != null) {
-            hero.setSharedElementName("hero");
-        }
+        setupHero();
         TransitionManager transitionManager = getContentTransitionManager();
         TransitionSet transitions = new TransitionSet();
         Fall fall = new Fall();
         fall.setDuration(600);
-        fall.setStartDelay(600);
-        fall.setHero(hero);
         transitions.addTransition(fall);
+        transitions.addTransition(new ScaleTransition());
+        transitions.addTransition(new ChangeBounds());
         transitions.addTransition(new Up());
-        transitionManager.setTransition("null", getContentScene(), transitions);
-
-        transitions = new TransitionSet();
-        mFall = new Fall();
-        mFall.setDuration(600);
-        transitions.addTransition(mFall);
-        transitions.addTransition(new Up());
-        transitionManager.setTransition(getContentScene(), "null", transitions);
+        transitionManager.setTransition(getContentScene(), transitions);
+        transitionManager.setExitTransition(getContentScene(), transitions);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mComeBack) {
-            mComeBack = false;
-            setContentView(R.layout.image_block);
+    private void setupHero() {
+        int id = getIntent().getIntExtra(KEY_ID, 0);
+        mHero = (ImageView) findViewById(id);
+        if (mHero != null) {
+            ArrayMap<String, String> sharedElementsMap = new ArrayMap<String, String>();
+            sharedElementsMap.put("hero", mHero.getSharedElementName());
+            getWindow().mapTransitionTargets(sharedElementsMap);
         }
-    }
-
-    private View getHero() {
-        Bundle transitionArgs = getTransitionArgs();
-        if (transitionArgs == null) {
-            return null;
-        }
-        int id = transitionArgs.getInt(KEY_ID);
-        return findViewById(id);
-    }
-
-    private static Property<Drawable, Rect> DRAWABLE_BOUNDS
-            = new Property<Drawable, Rect>(Rect.class, "bounds") {
-        @Override
-        public Rect get(Drawable object) {
-            return null;
-        }
-
-        @Override
-        public void set(Drawable object, Rect value) {
-            object.setBounds(value);
-            object.invalidateSelf();
-        }
-    };
-
-    @Override
-    public void startSharedElementTransition(Bundle transitionArgs) {
-        final ImageView hero = (ImageView)getHero();
-        hero.setSharedElementName(null);
-        hero.setVisibility(View.INVISIBLE);
-
-        int[] loc = new int[2];
-        hero.getLocationOnScreen(loc);
-        int endScreenLeft = loc[0];
-        int endScreenTop = loc[1];
-        int originalWidth = hero.getWidth();
-        int originalHeight = hero.getHeight();
-
-        hero.setVisibility(View.INVISIBLE);
-        ViewGroup sceneRoot = getContentScene().getSceneRoot();
-        sceneRoot.getLocationOnScreen(loc);
-        int overlayLeft = loc[0];
-        int overlayTop = loc[1];
-        final ViewGroupOverlay overlay = sceneRoot.getOverlay();
-
-        int endX = endScreenLeft - overlayLeft;
-        int endY = endScreenTop - overlayTop;
-
-        int startX = transitionArgs.getInt(KEY_LEFT_ON_SCREEN) - overlayLeft;
-        int startY = transitionArgs.getInt(KEY_TOP_ON_SCREEN) - overlayTop;
-        int startWidth = transitionArgs.getInt(KEY_WIDTH);
-        int startHeight = transitionArgs.getInt(KEY_HEIGHT);
-
-        int endHeight = originalWidth * startHeight / startWidth;
-        final Drawable image = hero.getDrawable();
-        Rect startBounds = new Rect(startX, startY, startX + startWidth, startY + startHeight);
-        endY += originalHeight - endHeight;
-        Rect endBounds = new Rect(endX, endY, endX + originalWidth, endY + endHeight);
-        ObjectAnimator boundsAnimator = ObjectAnimator.ofObject(image, DRAWABLE_BOUNDS,
-                new RectEvaluator(new Rect()), startBounds, endBounds);
-        hero.setImageDrawable(null);
-        image.setBounds(startBounds);
-        overlay.add(image);
-
-        boundsAnimator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                overlay.remove(image);
-                hero.setImageDrawable(image);
-                hero.setVisibility(View.VISIBLE);
-            }
-        });
-        boundsAnimator.start();
     }
 
     public void clicked(View v) {
-        v.setSharedElementName("hero");
-        mFall.setHero(v);
+        mHero = (ImageView) v;
         Intent intent = new Intent(this, ActivityTransitionDetails.class);
-        Bundle args = getHeroInfo(v);
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(args);
-        startActivity(intent, options.toBundle());
-        //v.setTranslationZ(300);
-        mComeBack = true;
+        intent.putExtra(KEY_ID, v.getId());
+        startActivity(intent, new Pair<View, String>(mHero, "hero"));
     }
 
     private int randomColor() {
@@ -184,15 +90,25 @@ public class ActivityTransition extends Activity {
         return 0xFF000000 | (red << 16) | (green << 8) | blue;
     }
 
-    static Bundle getHeroInfo(View view) {
-        int[] loc = new int[2];
-        view.getLocationOnScreen(loc);
-        Bundle bundle = new Bundle();
-        bundle.putInt(KEY_LEFT_ON_SCREEN, loc[0]);
-        bundle.putInt(KEY_TOP_ON_SCREEN, loc[1]);
-        bundle.putInt(KEY_WIDTH, view.getWidth());
-        bundle.putInt(KEY_HEIGHT, view.getHeight());
-        bundle.putInt(KEY_ID, view.getId());
-        return bundle;
+    @Override
+    public void onCaptureSharedElementStart(Transition transition) {
+        int width = mHero.getWidth();
+        int newTop = mHero.getBottom() - width;
+        mHero.setTop(newTop);
+
+        int imageWidth = mHero.getDrawable().getIntrinsicWidth();
+        mHero.setPivotX(0);
+        mHero.setPivotY(0);
+        float scale = ((float)width)/imageWidth;
+        mHero.setScaleX(scale);
+        mHero.setScaleY(scale);
+    }
+
+    @Override
+    public void onCaptureSharedElementEnd() {
+        mHero.setPivotX(0);
+        mHero.setPivotY(0);
+        mHero.setScaleX(1);
+        mHero.setScaleY(1);
     }
 }
