0faa47e06ddaf847996d0da3016279e2f099db4d
Andrew Hsieh <andrewhsieh@google.com>
2014-06-20 16:45:29 -0700

    Restore arm's machine/endian.h to be useful for ARMv5
    Revert bionic c54ca40aef48009e7b0e5b2b3069aad62ffd3453 because
    endian.h is seen by APP_ABI=armeabi as well.  Instruction
    "rev" and "rev16" doesn't exist until ARMv6
    
    Change-Id: I653740e3704b9b64e3e2a2e17452675635a14776

--- ndk/platforms/android-L/arch-arm/include/machine/endian.h
+++ ndk/platforms/android-L/arch-arm/include/machine/endian.h
@@ -33,6 +33,15 @@
 
 #ifdef __GNUC__
 
+/*
+ * REV and REV16 weren't available on ARM5 or ARM4.
+ * We don't include <machine/cpu-features.h> because it pollutes the
+ * namespace with macros like PLD.
+ */
+#if !defined __ARM_ARCH_5__ && !defined __ARM_ARCH_5T__ && \
+    !defined __ARM_ARCH_5TE__ && !defined __ARM_ARCH_5TEJ__ && \
+    !defined __ARM_ARCH_4T__ && !defined __ARM_ARCH_4__
+
 /* According to RealView Assembler User's Guide, REV and REV16 are available
  * in Thumb code and 16-bit instructions when used in Thumb-2 code.
  *
@@ -65,6 +74,7 @@
 /* Tell sys/endian.h we have MD variants of the swap macros.  */
 #define MD_SWAP
 
+#endif  /* __ARM_ARCH__ */
 #endif  /* __GNUC__ */
 
 #if defined(__ARMEB__)
