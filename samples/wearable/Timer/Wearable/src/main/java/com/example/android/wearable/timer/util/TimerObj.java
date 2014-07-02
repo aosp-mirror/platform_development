50532de03f8fdad7ba161cbc3a6092e1845ef2a9
Andy Wickham <awickham@google.com>
2014-07-02 14:23:37 -0700

    Add AOSP Copyright header to samples.
    Bug: 16039713
    Change-Id: I0aead1428c7c6a100214dabb85dbf357e37fcfc8

--- samples/wearable/Timer/Wearable/src/main/java/com/example/android/wearable/timer/util/TimerObj.java
+++ samples/wearable/Timer/Wearable/src/main/java/com/example/android/wearable/timer/util/TimerObj.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package com.example.android.wearable.timer.util;
 
 import android.os.SystemClock;
-
 
 /** This class represents a timer. */
 public class TimerObj {
