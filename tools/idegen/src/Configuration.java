e83d6400fb5f125cee1edb6ebb7663abe5cae6c4
Sky Faber <skyf@google.com>
2014-07-08 11:26:53 -0700

    idegen: Skip directories named *.java when parsing source.
    Change-Id: I8a6700ebcc198851b80de4832a43d147dc887f3a

--- tools/idegen/src/Configuration.java
+++ tools/idegen/src/Configuration.java
@@ -132,7 +132,7 @@ public class Configuration {
             String path = file.getPath().substring(2);
 
             // Keep track of source roots for .java files.
-            if (path.endsWith(".java")) {
+            if (path.endsWith(".java") && !file.isDirectory()) {
                 if (firstJavaFile) {
                     // Only parse one .java file per directory.
                     firstJavaFile = false;
