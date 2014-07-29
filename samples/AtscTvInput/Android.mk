4746c9797abba0cecf5563c0d330a6e2ba5dc512
Dongwon Kang <dwkang@google.com>
2014-07-29 14:56:06 +0900

    Remove AtscTvInput in favor of having another sample input under developers.
    - The real sample app for developers will be located under developers/ and
      managed by dev-rel team.
    
    Change-Id: I6d190b2c9a24ad64e79e3fd1c1c824abeda2eb3f

--- samples/AtscTvInput/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PACKAGE_NAME := AtscTvInput
-LOCAL_MODULE_TAGS := samples
-LOCAL_SDK_VERSION := current
-
-include $(BUILD_PACKAGE)
