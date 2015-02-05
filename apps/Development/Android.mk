4b952f0df30256146a1b6ba03761978bc982c305
Narayan Kamath <narayan@google.com>
2015-02-05 15:04:30 +0000

    Add an explicit dependency on org.apache.http.legacy.
    bug: 18027885
    Change-Id: Ie3a251fd63ec25a3c5cb46c80236f43724166331

--- apps/Development/Android.mk
+++ apps/Development/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
 
-LOCAL_JAVA_LIBRARIES := android.test.runner telephony-common
+LOCAL_JAVA_LIBRARIES := android.test.runner telephony-common org.apache.http.legacy
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
                 src/com/android/development/IRemoteService.aidl \
