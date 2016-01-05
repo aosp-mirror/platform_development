387b63df1953fc64d5e89bce1d5640f0bd69ac60
Elliott Hughes <enh@google.com>
2016-01-05 09:34:58 -0800

    Clean up etc1tool.
    Remove makefile cruft and fix all the compiler warnings.
    
    Change-Id: I24e40143cac47a566c191f4f1ca27df7b4f55821

--- tools/etc1tool/Android.mk
+++ tools/etc1tool/Android.mk
@@ -9,12 +9,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := etc1tool.cpp
 
-LOCAL_C_INCLUDES += external/libpng
-LOCAL_C_INCLUDES += external/zlib
-LOCAL_C_INCLUDES += build/libs/host/include
-LOCAL_C_INCLUDES += frameworks/base/opengl/include
-
-#LOCAL_WHOLE_STATIC_LIBRARIES := 
 LOCAL_STATIC_LIBRARIES := \
 	libhost \
 	libexpat \
