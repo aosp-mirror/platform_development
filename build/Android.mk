6f5779692a37a0261f0d1e224ce3b9a1a4579751
Yohann Roussel <yroussel@google.com>
2016-03-17 12:00:04 +0100

    Package Jack and Jill directly from prebuilt
    Instead of taking it from out.
    
    Bug: 27372042
    Change-Id: Ie266104e51c09fa21ccb09c1f27ad7068472965c

--- build/Android.mk
+++ build/Android.mk
@@ -84,8 +84,8 @@ ALL_SDK_FILES += $(android_jar_full_target)
 # ====================================================
 
 # The Jack & Jill compiler jars
-ALL_SDK_FILES += $(HOST_OUT)/framework/jack.jar
-ALL_SDK_FILES += $(HOST_OUT)/framework/jill.jar
+ALL_SDK_FILES += prebuilts/sdk/tools/jacks/jack-$(JACK_SDKTOOL_VERSION).jar
+ALL_SDK_FILES += prebuilts/sdk/tools/jills/jill-$(JACK_SDKTOOL_VERSION).jar
 
 # The uiautomator stubs
 ALL_SDK_FILES += $(TARGET_OUT_COMMON_INTERMEDIATES)/JAVA_LIBRARIES/android_uiautomator_intermediates/javalib.jar
