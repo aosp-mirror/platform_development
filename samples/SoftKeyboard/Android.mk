59581fb6d478cfee517a078aea2d5c002f4c7fb9
bohu <bohu@google.com>
2016-04-26 12:46:13 -0700

    Emulator: enable predexopt for some sample apps
    Some sample apps on emulator are placed on /data
    partition and are not pre-dexopted.
    Enable them to make the emulator system image a
    little faster, especially for arm.
    bug: 28312444
    
    This should only impact SDK system images, not
    real devices.
    
    Change-Id: Icdb34512a3a6d95b4d6ffd2415835c6ba8c77fcc

--- samples/SoftKeyboard/Android.mk
+++ samples/SoftKeyboard/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := samples
-
+LOCAL_DEX_PREOPT := true
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_SDK_VERSION := current
