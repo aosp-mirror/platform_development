ce69622596a4004a56ea58a2d19d84897a6d7762
Kirill Grouchnikov <kirillg@google.com>
2016-05-02 13:56:18 -0400

    Merge changes necessary to build refactored support library

--- build/Android.mk
+++ build/Android.mk
@@ -122,6 +122,11 @@ endef
 
 ANDROID_SUPPORT_LIBRARIES := \
     android-support-annotations \
+    android-support-compat \
+    android-support-media-compat \
+    android-support-core-ui \
+    android-support-fragment \
+    android-support-core-utils \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-cardview \
