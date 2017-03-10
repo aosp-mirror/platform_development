c25a1ed85ded97e21378ba2a584502a4c9f99f15
Logan Chien <loganchien@google.com>
2017-03-10 18:31:14 +0800

    vndk-def: Fix vndk-ext computation.
    This commit fixes vndk-fwk-ext qand vndk-vnd-ext computation.  We should
    hash imported_lib, an instance of ELFLinkData, instead of ELF object.
    
    Test: ./tests/test_vndk.py
    Change-Id: Idc5123bbf9980b76527b5fa295a1fe01c8f7f44c

--- vndk/tools/definition-tool/tests/test_vndk.py
+++ vndk/tools/definition-tool/tests/test_vndk.py
@@ -9,7 +9,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
 
 from compat import StringIO
-from vndk_definition_tool import ELF, ELFLinker, PT_SYSTEM, PT_VENDOR
+from vndk_definition_tool import (ELF, ELFLinker, PT_SYSTEM, PT_VENDOR,
+                                  GenericRefs)
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 TESTDATA_DIR = os.path.join(SCRIPT_DIR ,'testdata', 'test_vndk')
@@ -41,6 +42,69 @@ class ELFLinkerVNDKTest(unittest.TestCase):
                          self._get_paths_from_nodes(vndk.vndk_core))
         self.assertEqual([], self._get_paths_from_nodes(vndk.vndk_fwk_ext))
         self.assertEqual([], self._get_paths_from_nodes(vndk.vndk_vnd_ext))
+
+    def test_compute_vndk_fwk_ext(self):
+        class MockBannedLibs(object):
+            def is_banned(self, name):
+                return False
+
+        generic_refs_dir = os.path.join(TESTDATA_DIR, 'vndk_ext_ref')
+
+        generic_refs = GenericRefs.create_from_dir(generic_refs_dir)
+
+        input_dir = os.path.join(TESTDATA_DIR, 'vndk_fwk_ext')
+
+        graph = ELFLinker.create_from_dump(
+                system_dirs=[os.path.join(input_dir, 'system')],
+                vendor_dirs=[os.path.join(input_dir, 'vendor')],
+                generic_refs=generic_refs)
+
+        vndk = graph.compute_vndk(sp_hals=set(), vndk_stable=set(),
+                                  vndk_customized_for_system=set(),
+                                  vndk_customized_for_vendor=set(),
+                                  generic_refs=generic_refs,
+                                  banned_libs=MockBannedLibs())
+
+        self.assertEqual(['/system/lib/vndk/libRS.so',
+                          '/system/lib/vndk/libcutils.so',
+                          '/system/lib64/vndk/libRS.so',
+                          '/system/lib64/vndk/libcutils.so'],
+                         self._get_paths_from_nodes(vndk.vndk_core))
+        self.assertEqual(['/system/lib/libRS.so',
+                          '/system/lib64/libRS.so'],
+                         self._get_paths_from_nodes(vndk.vndk_fwk_ext))
+        self.assertEqual([], self._get_paths_from_nodes(vndk.vndk_vnd_ext))
+
+    def test_compute_vndk_vnd_ext(self):
+        class MockBannedLibs(object):
+            def is_banned(self, name):
+                return False
+
+        generic_refs_dir = os.path.join(TESTDATA_DIR, 'vndk_ext_ref')
+
+        generic_refs = GenericRefs.create_from_dir(generic_refs_dir)
+
+        input_dir = os.path.join(TESTDATA_DIR, 'vndk_vnd_ext')
+
+        graph = ELFLinker.create_from_dump(
+                system_dirs=[os.path.join(input_dir, 'system')],
+                vendor_dirs=[os.path.join(input_dir, 'vendor')],
+                generic_refs=generic_refs)
+
+        vndk = graph.compute_vndk(sp_hals=set(), vndk_stable=set(),
+                                  vndk_customized_for_system=set(),
+                                  vndk_customized_for_vendor=set(),
+                                  generic_refs=generic_refs,
+                                  banned_libs=MockBannedLibs())
+
+        self.assertEqual(['/system/lib/vndk/libRS.so',
+                          '/system/lib/vndk/libcutils.so',
+                          '/system/lib64/vndk/libRS.so',
+                          '/system/lib64/vndk/libcutils.so'],
+                         self._get_paths_from_nodes(vndk.vndk_core))
+        self.assertEqual([], self._get_paths_from_nodes(vndk.vndk_fwk_ext))
+        self.assertEqual(['/system/lib/libRS.so', '/system/lib64/libRS.so'],
+                         self._get_paths_from_nodes(vndk.vndk_vnd_ext))
 
 
 if __name__ == '__main__':
