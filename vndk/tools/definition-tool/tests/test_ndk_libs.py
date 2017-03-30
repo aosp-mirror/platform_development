b622618f8d472a3cae21a9d58c96d28bb3c0a22e
Logan Chien <loganchien@google.com>
2017-03-30 20:07:36 +0800

    vndk-def: Add NDKLibDict.classify().
    This commit adds NDKLibDict.classify() so that we can get the type of
    NDK libraries (e.g. LL-NDK, SP-NDK, or HL-NDK.)
    
    Test: ./tests/run.py
    Change-Id: I332af0d50a403a719c4b4bb586c6cb20f925bcc4
    (cherry picked from commit 0b455c6732232e26ed44c0150e7d60cf3f61948a)

--- vndk/tools/definition-tool/tests/test_ndk_libs.py
+++ vndk/tools/definition-tool/tests/test_ndk_libs.py
@@ -12,83 +12,83 @@ from vndk_definition_tool import NDK_LIBS
 
 
 class NDKLibDictTest(unittest.TestCase):
-    def test_is_llndk(self):
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/libc.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/libdl.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/liblog.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/libm.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/libstdc++.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib/libz.so'))
+    def test_is_ll_ndk(self):
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/libc.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/libdl.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/liblog.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/libm.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/libstdc++.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib/libz.so'))
 
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/libc.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/libdl.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/liblog.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/libm.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/libstdc++.so'))
-        self.assertTrue(NDK_LIBS.is_llndk('/system/lib64/libz.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/libc.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/libdl.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/liblog.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/libm.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/libstdc++.so'))
+        self.assertTrue(NDK_LIBS.is_ll_ndk('/system/lib64/libz.so'))
 
-        self.assertFalse(NDK_LIBS.is_llndk('/system/lib/libz'))
-        self.assertFalse(NDK_LIBS.is_llndk('/system/lib/libzz.so'))
+        self.assertFalse(NDK_LIBS.is_ll_ndk('/system/lib/libz'))
+        self.assertFalse(NDK_LIBS.is_ll_ndk('/system/lib/libzz.so'))
 
-    def test_is_spndk(self):
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib/libEGL.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib/libGLESv1_CM.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib/libGLESv2.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib/libGLESv3.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib/libvulkan.so'))
+    def test_is_sp_ndk(self):
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib/libEGL.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib/libGLESv1_CM.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib/libGLESv2.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib/libGLESv3.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib/libvulkan.so'))
 
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib64/libEGL.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib64/libGLESv1_CM.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib64/libGLESv2.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib64/libGLESv3.so'))
-        self.assertTrue(NDK_LIBS.is_spndk('/system/lib64/libvulkan.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib64/libEGL.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib64/libGLESv1_CM.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib64/libGLESv2.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib64/libGLESv3.so'))
+        self.assertTrue(NDK_LIBS.is_sp_ndk('/system/lib64/libvulkan.so'))
 
         # Vendor libraries with the same name are still not SP-NDK.
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/libEGL.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/libGLESv1_CM.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/libGLESv2.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/libGLESv3.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/libEGL.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/libGLESv1_CM.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/libGLESv2.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/libGLESv3.so'))
 
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/egl/libEGL.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/egl/libGLESv1_CM.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/egl/libGLESv2.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/egl/libGLESv3.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/egl/libEGL.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/egl/libGLESv1_CM.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/egl/libGLESv2.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/egl/libGLESv3.so'))
 
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/libvulkan.so'))
-        self.assertFalse(NDK_LIBS.is_spndk('/vendor/lib64/vulkan.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/libvulkan.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/vendor/lib64/vulkan.so'))
 
         # LL-NDK is not SP-NDK.
-        self.assertFalse(NDK_LIBS.is_spndk('/system/lib/libc.so'))
+        self.assertFalse(NDK_LIBS.is_sp_ndk('/system/lib/libc.so'))
 
     def test_is_hlndk(self):
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libOpenMAXAL.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libOpenSLES.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libandroid.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libcamera2ndk.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libjnigraphics.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib/libmediandk.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libOpenMAXAL.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libOpenSLES.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libandroid.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libcamera2ndk.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libjnigraphics.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib/libmediandk.so'))
 
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libOpenMAXAL.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libOpenSLES.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libandroid.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libcamera2ndk.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libjnigraphics.so'))
-        self.assertTrue(NDK_LIBS.is_hlndk('/system/lib64/libmediandk.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libOpenMAXAL.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libOpenSLES.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libandroid.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libcamera2ndk.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libjnigraphics.so'))
+        self.assertTrue(NDK_LIBS.is_hl_ndk('/system/lib64/libmediandk.so'))
 
         # LL-NDK and SP-NDK are not HL-NDK.
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libc.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libEGL.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libGLESv1_CM.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libGLESv2.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libGLESv3.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib/libvulkan.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libc.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libEGL.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libGLESv1_CM.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libGLESv2.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libGLESv3.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib/libvulkan.so'))
 
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libc.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libEGL.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libGLESv1_CM.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libGLESv2.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libGLESv3.so'))
-        self.assertFalse(NDK_LIBS.is_hlndk('/system/lib64/libvulkan.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libc.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libEGL.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libGLESv1_CM.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libGLESv2.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libGLESv3.so'))
+        self.assertFalse(NDK_LIBS.is_hl_ndk('/system/lib64/libvulkan.so'))
 
     def test_is_ndk(self):
         # LL-NDK
@@ -133,6 +133,25 @@ class NDKLibDictTest(unittest.TestCase):
         self.assertTrue(NDK_LIBS.is_ndk('/system/lib64/libjnigraphics.so'))
         self.assertTrue(NDK_LIBS.is_ndk('/system/lib64/libmediandk.so'))
         self.assertTrue(NDK_LIBS.is_ndk('/system/lib64/libvulkan.so'))
+
+    def test_classify(self):
+        self.assertEqual(NDK_LIBS.NOT_NDK,
+                         NDK_LIBS.classify('/system/lib/libfoo.so'))
+        self.assertEqual(NDK_LIBS.LL_NDK,
+                         NDK_LIBS.classify('/system/lib/libc.so'))
+        self.assertEqual(NDK_LIBS.SP_NDK,
+                         NDK_LIBS.classify('/system/lib/libEGL.so'))
+        self.assertEqual(NDK_LIBS.HL_NDK,
+                         NDK_LIBS.classify('/system/lib/libmediandk.so'))
+
+        self.assertEqual(NDK_LIBS.NOT_NDK,
+                         NDK_LIBS.classify('/system/lib64/libfoo.so'))
+        self.assertEqual(NDK_LIBS.LL_NDK,
+                         NDK_LIBS.classify('/system/lib64/libc.so'))
+        self.assertEqual(NDK_LIBS.SP_NDK,
+                         NDK_LIBS.classify('/system/lib64/libEGL.so'))
+        self.assertEqual(NDK_LIBS.HL_NDK,
+                         NDK_LIBS.classify('/system/lib64/libmediandk.so'))
 
 
 if __name__ == '__main__':
