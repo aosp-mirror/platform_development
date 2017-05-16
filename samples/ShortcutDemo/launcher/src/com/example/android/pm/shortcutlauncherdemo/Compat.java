d057d9f02759621e112409149ee0194a7796509d
Makoto Onuki <omakoto@google.com>
2017-05-16 12:05:37 -0700

    The test launcher should work on N-MR2 too.
    Fixes 38345873
    Test: manual
    Change-Id: I72cedfe860c0c57672d057b209df03917ed6411c

---
+++ samples/ShortcutDemo/launcher/src/com/example/android/pm/shortcutlauncherdemo/Compat.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.pm.shortcutlauncherdemo;
+
+import android.content.Context;
+import android.content.pm.LauncherApps;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.support.v4.os.BuildCompat;
+
+import java.util.List;
+
+public class Compat {
+    public static List<UserHandle> getProfiles(Context context) {
+        if (BuildCompat.isAtLeastO()) {
+            return context.getSystemService(LauncherApps.class).getProfiles();
+        } else {
+            return context.getSystemService(UserManager.class).getUserProfiles();
+        }
+    }
+}
