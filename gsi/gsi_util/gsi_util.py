a30d9ef26b0e5ff66bfa843244ac1f85c9442d77
SzuWei Lin <szuweilin@google.com>
2017-12-27 18:03:20 +0800

    gsi_util: adding check_compat subcommand
    'check_compat' command can check the compatibility between
    system and vendor image, which can be any source supported by
    mounters, ex. image file, folder or adb.
    
    Uses following command for the detail:
    
        $ ./gsu_util.py check_compat --help
    
    The patch also includes a 'checker' framework. There is only
    one checker 'VintfChecker' at this time. VintfChecker uses a
    host tool, 'checkvintf', to check the compatibility.
    
    Bug: 70253825
    Test: check_compat with different mounters
    Change-Id: I459b4cbd38465c0058087b4c68bca66e491c940e

--- gsi/gsi_util/gsi_util.py
+++ gsi/gsi_util/gsi_util.py
@@ -28,7 +28,7 @@ class GsiUtil(object):
 
   # Adds gsi_util COMMAND here.
   # TODO(bowgotsai): auto collect from gsi_util/commands/*.py
-  _COMMANDS = ['flash_gsi', 'pull', 'dump', 'hello']
+  _COMMANDS = ['flash_gsi', 'pull', 'dump', 'check_compat', 'hello']
 
   _LOGGING_FORMAT = '%(message)s'
   _LOGGING_LEVEL = logging.WARNING
