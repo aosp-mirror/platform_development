7138f343a135aecab3c99d8729ceb162dc87cad2
chaviw <chaviw@google.com>
2018-07-16 16:21:24 -0700

    Use windowFrames.frame instead of frame to draw the area.
    The code is changed so the frame protobuf is now under another class
    called windowFrames. Update the js code so it can properly draw the
    frame with the new code change.
    
    Test: Ran on localhost with updated protobuf file
    Bug: 111359087
    Change-Id: I072daae54601120de5b6eaa0a64ee7c3096e4a43

--- tools/winscope/src/transform_wm.js
+++ tools/winscope/src/transform_wm.js
@@ -31,7 +31,7 @@ function transform_window(entry) {
     }
   }
   var name = renderIdentifier(entry.identifier)
-  var rect = transform_rect(entry.frame, name);
+  var rect = transform_rect(entry.windowFrames.frame, name);
 
   if (visible) {
     chips.push(get_visible_chip());
