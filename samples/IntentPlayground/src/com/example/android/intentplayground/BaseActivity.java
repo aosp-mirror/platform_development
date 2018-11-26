a2f0b5d456b551f62ea28084858bbe92ea35904b
Liam Clark <liamclark@google.com>
2018-11-26 14:25:57 -0800

    Move launch default activity prompt to the menu
    Instead of asking at startup, the prompt now launches from a menu item.
    This also get's rid of the snackbar because it was causing bugs with
    rotation.
    
    Furthermore now the android.permission.SYSTEM_ALERT_WINDOW permission is
    no longer needed.
    
    Test: Manual
    Change-Id: I566e58d4844aecf127b5198c050ff115de7e4919

--- samples/IntentPlayground/src/com/example/android/intentplayground/BaseActivity.java
+++ samples/IntentPlayground/src/com/example/android/intentplayground/BaseActivity.java
@@ -16,20 +16,25 @@
 
 package com.example.android.intentplayground;
 
+import static com.example.android.intentplayground.Node.newTaskNode;
+
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import java.util.ArrayList;
 
 /**
@@ -39,10 +44,12 @@ public abstract class BaseActivity extends AppCompatActivity implements
         IntentBuilderView.OnLaunchCallback {
     public final static String EXTRA_LAUNCH_FORWARD = "com.example.android.launchForward";
     public final static String BUILDER_VIEW = "com.example.android.builderFragment";
-    public static final String TREE_FRAGMENT =  "com.example.android.treeFragment";
+    public static final String TREE_FRAGMENT = "com.example.android.treeFragment";
     public static final String EXPECTED_TREE_FRAGMENT = "com.example.android.expectedTreeFragment";
     public static final int LAUNCH_REQUEST_CODE = 0xEF;
+
     public enum Mode {LAUNCH, VERIFY, RESULT}
+
     public boolean userLeaveHintWasCalled = false;
     protected Mode mStatus = Mode.LAUNCH;
 
@@ -53,7 +60,9 @@ public abstract class BaseActivity extends AppCompatActivity implements
         if (BuildConfig.DEBUG) Log.d(getLocalClassName(), "onCreate()");
         // Setup action bar
         Toolbar appBar = findViewById(R.id.app_bar);
+        appBar.setTitle(this.getClass().getSimpleName());
         setSupportActionBar(appBar);
+
         loadMode(Mode.LAUNCH);
     }
 
@@ -68,6 +77,7 @@ public abstract class BaseActivity extends AppCompatActivity implements
 
     /**
      * Initializes the UI for the specified {@link Mode}.
+     *
      * @param mode The mode to display.
      */
     protected void loadMode(Mode mode) {
@@ -129,8 +139,55 @@ public abstract class BaseActivity extends AppCompatActivity implements
             case R.id.app_bar_test:
                 runIntentTests();
                 break;
+            case R.id.app_bar_launch:
+                askToLaunchTasks();
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void askToLaunchTasks() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage(R.string.launch_explanation)
+                .setTitle(R.string.ask_to_launch)
+                .setPositiveButton(R.string.ask_to_launch_affirm, (dialogInterface, i) -> {
+                    setupTaskPreset().startActivities(TestBase.LaunchStyle.TASK_STACK_BUILDER);
+                    dialogInterface.dismiss();
+                })
+                .setNegativeButton(R.string.ask_to_launch_cancel, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                })
+                .create();
+        dialog.show();
+    }
+
+    protected TestBase setupTaskPreset() {
+        Node mRoot = Node.newRootNode();
+        // Describe initial setup of tasks
+        // create singleTask, singleInstance, and two documents in separate tasks
+        Node singleTask = newTaskNode()
+                .addChild(new Node(new ComponentName(this, SingleTaskActivity.class)));
+        Node docLaunchAlways = newTaskNode()
+                .addChild(new Node(new ComponentName(this, DocumentLaunchAlwaysActivity.class)));
+        Node docLaunchInto = newTaskNode()
+                .addChild(new Node(new ComponentName(this, DocumentLaunchIntoActivity.class)));
+        // Create three t0asks with three activities each, with affinity set
+        Node taskAffinity1 = newTaskNode()
+                .addChild(new Node(new ComponentName(this, TaskAffinity1Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity1Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity1Activity.class)));
+        Node taskAffinity2 = newTaskNode()
+                .addChild(new Node(new ComponentName(this, TaskAffinity2Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity2Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity2Activity.class)));
+        Node taskAffinity3 = newTaskNode()
+                .addChild(new Node(new ComponentName(this, TaskAffinity3Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity3Activity.class)))
+                .addChild(new Node(new ComponentName(this, TaskAffinity3Activity.class)));
+        mRoot.addChild(singleTask).addChild(docLaunchAlways).addChild(docLaunchInto)
+                .addChild(taskAffinity1).addChild(taskAffinity2).addChild(taskAffinity3);
+        return new TestBase(this, mRoot);
     }
 
     protected void runIntentTests() {
@@ -160,7 +217,7 @@ public abstract class BaseActivity extends AppCompatActivity implements
                 R.id.build_intent_view);
         demo.addStep(R.string.help_step_four, R.id.fragment_container_bottom,
                 R.id.launch_button);
-        demo.setScroller((ScrollView) findViewById(R.id.scroll_container));
+        demo.setScroller(findViewById(R.id.scroll_container));
         demo.setOnFinish(() -> container.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE));
         fragmentManager.beginTransaction()
                 .add(R.id.root_container, demo)
