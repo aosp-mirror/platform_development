f767eb4aa92dd6b4a864ed4e46dba63f75e41338
Nick Cook <nickcook@google.com>
2019-07-18 22:27:08 +0000

    docs: Fix outdated sample code in Context#getExternalFilesDir. Test: make ds-docs
    Bug: 129290716
    Change-Id: I0b1c1d9222c96c6bb2777f58d273546c81d77eb2

--- samples/ApiDemos/src/com/example/android/apis/content/ExternalStorage.java
+++ samples/ApiDemos/src/com/example/android/apis/content/ExternalStorage.java
@@ -339,19 +339,14 @@ public class ExternalStorage extends Activity {
          // Get path for the file on external storage.  If external
          // storage is not currently mounted this will fail.
          File file = new File(getExternalFilesDir(null), "DemoFile.jpg");
-         if (file != null) {
-             file.delete();
-         }
+         file.delete();
      }
 
      boolean hasExternalStoragePrivateFile() {
          // Get path for the file on external storage.  If external
          // storage is not currently mounted this will fail.
          File file = new File(getExternalFilesDir(null), "DemoFile.jpg");
-         if (file != null) {
-             return file.exists();
-         }
-         return false;
+         return file.exists();
      }
  // END_INCLUDE(private_file)
 
