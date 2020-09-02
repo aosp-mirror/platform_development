9d56e69e38ec3ecb1f67db97b494d3f3891e27e2
Pablo Gamito <pablogamito@google.com>
2020-09-02 13:04:28 +0200

    Update webpack config to inline js and css
    This is required for production builds since we only deploy a single .html file and don't server the rest of the generated files
    
    Test: Try and build and see if Winscope loads by loading only the generated .html file
    Change-Id: Idc7614d72bb91a35da3574f5c93588690ccfb6a0

--- tools/winscope/webpack.config.prod.js
+++ tools/winscope/webpack.config.prod.js
@@ -17,15 +17,15 @@
 'use strict';
 
 const webpack = require('webpack');
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const path = require('path');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 const isProd = process.env.NODE_ENV === 'production';
-const environment = isProd ? require('./env/prod.env') : require('./env/staging.env');
+const environment =
+  isProd ? require('./env/prod.env') : require('./env/staging.env');
 
 
 const webpackConfig = merge(commonConfig, {
@@ -34,21 +34,16 @@ const webpackConfig = merge(commonConfig, {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'js/[hash].js',
-    chunkFilename: 'js/[id].[hash].chunk.js'
+    chunkFilename: 'js/[id].[hash].chunk.js',
   },
   optimization: {
     runtimeChunk: 'single',
     minimizer: [
       new OptimizeCSSAssetsPlugin({
         cssProcessorPluginOptions: {
-          preset: ['default', { discardComments: { removeAll: true } }],
-        }
+          preset: ['default', {discardComments: {removeAll: true}}],
+        },
       }),
-      // new UglifyJSPlugin({
-      //   cache: true,
-      //   parallel: true,
-      //   sourceMap: !isProd
-      // })
     ],
     splitChunks: {
       chunks: 'all',
@@ -58,41 +53,43 @@ const webpackConfig = merge(commonConfig, {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            const packageName = module.context
+                .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
             return `npm.${packageName.replace('@', '')}`;
-          }
+          },
         },
         styles: {
           test: /\.css$/,
           name: 'styles',
           chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+          enforce: true,
+        },
+      },
+    },
   },
   plugins: [
     new webpack.EnvironmentPlugin(environment),
     new MiniCSSExtractPlugin({
       filename: 'css/[name].[hash].css',
-      chunkFilename: 'css/[id].[hash].css'
+      chunkFilename: 'css/[id].[hash].css',
     }),
     new CompressionPlugin({
       filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp('\\.(js|css)$'),
       threshold: 10240,
-      minRatio: 0.8
+      minRatio: 0.8,
     }),
-    new webpack.HashedModuleIdsPlugin()
-  ]
+    new webpack.HashedModuleIdsPlugin(),
+  ],
 });
 
 if (!isProd) {
   webpackConfig.devtool = 'source-map';
 
   if (process.env.npm_config_report) {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+    const BundleAnalyzerPlugin =
+      require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
     webpackConfig.plugins.push(new BundleAnalyzerPlugin());
   }
 }
