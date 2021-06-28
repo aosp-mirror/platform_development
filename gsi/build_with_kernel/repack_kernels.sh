29c002699b6b8ba678b0407c7e62af989e411b09
SzuWei Lin <szuweilin@google.com>
2021-06-28 14:45:45 +0800

    Add more files to dist for build chaining
    Includes:
    
    - Provide the build number of the source kernel
      - prebuilt-info.txt
    - For integrating mixed build:
      - System.map
      - vmlinux.symvers
      - modules.builtin
      - modules.builtin.modinfo
    
    Bug: 191439845
    Test: TH
    Change-Id: I48220978bcbe38faa8fc5e408a1e01aa0638da6a

--- gsi/build_with_kernel/repack_kernels.sh
+++ gsi/build_with_kernel/repack_kernels.sh
@@ -18,6 +18,33 @@ source development/gsi/build_with_kernel/bld-gki.sh
 
 set -e
 
+if [[ -z "${DIST_DIR}" ]]; then
+  echo "DIST_DIR must be defined." 1>&2
+  exit 1
+fi
+
+# Path for kernel 5.10
+PREBUILTS_ROOT="out/prebuilt_cached/artifacts/common-android12-5_10-kernel_aarch64"
+DIST_ROOT="${DIST_DIR}/kernel/5.10"
+
+# Pack all compress format for kernel images
 repack \
-  "out/prebuilt_cached/artifacts/common-android12-5_10-kernel_aarch64/Image" \
+  "${PREBUILTS_ROOT}/Image" \
   "${OUT_DIR}/target/kernel/5.10/arm64" 5.10
+
+# Prepare the dist folder
+mkdir -p "${DIST_ROOT}"
+
+# Prepare prebuilt-info.txt
+BID=$(cat "${PREBUILTS_ROOT}/BUILD_INFO" | sed -n 's/^.*"bid":\s*"\(.*\)".*$/\1/p')
+cat > "${DIST_ROOT}/prebuilt-info.txt" <<EOF
+{
+    "kernel-build-id": ${BID}
+}
+EOF
+
+# Copy all other helper files
+cp "${PREBUILTS_ROOT}/System.map" "${DIST_ROOT}"
+cp "${PREBUILTS_ROOT}/vmlinux.symvers" "${DIST_ROOT}"
+cp "${PREBUILTS_ROOT}/modules.builtin" "${DIST_ROOT}"
+cp "${PREBUILTS_ROOT}/modules.builtin.modinfo" "${DIST_ROOT}"
