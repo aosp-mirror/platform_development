578160457278f2881b58680d34afe4d2155a36fa
lishutong <lishutong@google.com>
2021-07-13 03:19:32 +0000

    Use material design for OTAGUI.
    1. Changed the layout using material design ui in vuetify.
    2. Changed the file upload to drag-and-drop style.
    
    Test: Mannual tested.
    Change-Id: I8b34a405ae3783d6806946f0adc5fc5ac9c30a49

--- tools/otagui/src/App.vue
+++ tools/otagui/src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'Create' }">
-        Create Jobs
-      </router-link> |
-      <router-link :to="{ name: 'JobList' }">
-        Jobs Status
-      </router-link> |
-      <router-link :to="{ name: 'About' }">
-        About
-      </router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar
+      rounded
+      color="primary"
+    >
+      <v-app-bar-title> OTA Dashboard </v-app-bar-title>
+      <v-spacer />
+      <v-btn
+        v-for="link in links"
+        :key="`${link}-header-link`"
+        :to="{ name: link }"
+        class="ml-5"
+        color="primary"
+      >
+        {{ link }}
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: ['Create', 'JobList', 'About'],
+    }
+  },
+}
+</script>
+
+<style>
+h3 {
+  text-align: center;
+}
+</style>
