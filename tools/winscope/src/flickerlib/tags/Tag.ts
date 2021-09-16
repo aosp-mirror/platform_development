e632742540cd66fd67c91d5e846a1541d970769c
Priyanka <priyankaspatel@google.com>
2021-09-16 14:27:13 +0000

    Added display support for PIP_CLOSE.
    Add PIP_CLOSE to transitions supported by display.
    
    Bug: b/199882679
    
    Test: Upon merge of PipCloseProcessor, test display.
    
    Change-Id: I2856a6b74b0ffe680f6bb978322ef3ceb98d267f

--- tools/winscope/src/flickerlib/tags/Tag.ts
+++ tools/winscope/src/flickerlib/tags/Tag.ts
@@ -22,6 +22,7 @@ const transitionTypeMap = new Map([
   ['ROTATION', TransitionType.ROTATION],
   ['PIP_ENTER', TransitionType.PIP_ENTER],
   ['PIP_RESIZE', TransitionType.PIP_RESIZE],
+  ['PIP_CLOSE', TransitionType.PIP_CLOSE],
   ['PIP_EXIT', TransitionType.PIP_EXIT],
   ['APP_LAUNCH', TransitionType.APP_LAUNCH],
   ['APP_CLOSE', TransitionType.APP_CLOSE],
