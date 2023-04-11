108c36c52beb300c5180386d00ef23e0157b1e6b
Junghoon Jang <junghoonjang@google.com>
2023-04-11 09:56:58 +0000

    repo-pull: Introduce --current-branch argument
    To make the script can be used from automatic scripts, introduce
    `--current-branch` argument which allows pulling commits to the current
    branch without creating a local branch. It will skip the prompt which
    asking about the creation of a local branch.
    
    Test: With --current-branch argument, the script doesn't ask setting
      local branch name.
    Change-Id: Iff3cdebe6338bb2be738e60047d5c4dd81cde4f0
    Signed-off-by: Junghoon Jang <junghoonjang@google.com>

--- tools/repo_pull/repo_pull.py
+++ tools/repo_pull/repo_pull.py
@@ -409,6 +409,9 @@ def _parse_args():
     parser.add_argument('-j', '--parallel', default=1, type=int,
                         help='Number of parallel running commands')
 
+    parser.add_argument('--current-branch', action='store_true',
+                        help='Pull commits to the current branch')
+
     return parser.parse_args()
 
 
@@ -421,7 +424,7 @@ def _get_change_lists_from_args(args):
 
 def _get_local_branch_name_from_args(args):
     """Get the local branch name from args."""
-    if not args.branch and not _confirm(
+    if not args.branch and not args.current_branch and not _confirm(
             'Do you want to continue without local branch name?', False):
         print('error: `-b` or `--branch` must be specified', file=sys.stderr)
         sys.exit(1)
