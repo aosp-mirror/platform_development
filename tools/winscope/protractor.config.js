8191e28e73953d0ffa2fd0a8808d7adf3afd0609
Pablo Gamito <pablogamito@google.com>
2023-07-12 11:32:40 +0000

    Don't break after a succesful parse with a traces parser
    Even if one traces parser can parse the traces doesn't mean the next parser can't also do that same.
    
    Fixes: 290900126
    Change-Id: Ia1064fefaeeeea44c4452d88b27819446baf3710

--- tools/winscope/protractor.config.js
+++ tools/winscope/protractor.config.js
@@ -30,7 +30,7 @@ exports.config = {
       args: ['--headless', '--disable-gpu', '--window-size=1280x1024'],
     },
   },
-  chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_114.0.5735.90',
+  chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_114.0.5735.198',
 
   allScriptsTimeout: 10000,
   getPageTimeout: 10000,
