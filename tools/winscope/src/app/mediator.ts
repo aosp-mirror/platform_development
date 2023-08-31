e32735e391e7d43c9a4116176f97fb6bad1df932
Kean Mariotti <keanmariotti@google.com>
2023-08-31 15:35:54 +0000

    Fix handling of dumps with invalid timestamps
    Dumps with invalid timestamps (value 0) are filtered out
    from TimelineData, otherwise they would mess up the timeline
    presentation. In some cases this could prevent TracePipeline to
    retrieve (through TimelineData) a TracePosition to be used
    for viewers initialization. E.g. viewers would remain
    uninitialized when only dump traces were loaded.
    
    This commit handles the corner case in which only dump traces
    with invalid timestamps are loaded. With this change TracePipeline
    falls back to pick any timestamp from the loaded traces and uses
    it for viewers initialization.
    
    Bug: 262243662
    Test: npm run test:presubmit
    Change-Id: Id22e3679c4d3308867a44962ff67682df16cb3dd

--- tools/winscope/src/app/mediator.ts
+++ tools/winscope/src/app/mediator.ts
@@ -289,8 +289,8 @@ export class Mediator {
       })
     );
 
-    // Set position as soon as the viewers are created
-    await this.propagateTracePosition(this.timelineData.getCurrentPosition(), true);
+    // Set position to initialize viewers as soon as they are created
+    await this.propagateTracePosition(this.getTracePositionForViewersInitialization(), true);
 
     this.appComponent.onTraceDataLoaded(this.viewers);
     this.isTraceDataVisualized = true;
@@ -298,6 +298,34 @@ export class Mediator {
     if (this.lastRemoteToolTimestampReceived !== undefined) {
       await this.onRemoteTimestampReceived(this.lastRemoteToolTimestampReceived);
     }
+  }
+
+  private getTracePositionForViewersInitialization(): TracePosition | undefined {
+    const position = this.timelineData.getCurrentPosition();
+    if (position) {
+      return position;
+    }
+
+    // TimelineData might not provide a TracePosition because all the loaded traces are
+    // dumps with invalid timestamps (value zero). In this case let's create a TracePosition
+    // out of any timestamp from the loaded traces (if available).
+    const firstTimestamps = this.tracePipeline
+      .getTraces()
+      .mapTrace((trace) => {
+        if (trace.lengthEntries > 0) {
+          return trace.getEntry(0).getTimestamp();
+        }
+        return undefined;
+      })
+      .filter((timestamp) => {
+        return timestamp !== undefined;
+      }) as Timestamp[];
+
+    if (firstTimestamps.length > 0) {
+      return TracePosition.fromTimestamp(firstTimestamps[0]);
+    }
+
+    return undefined;
   }
 
   private resetAppToInitialState() {
