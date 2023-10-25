677c53e0893c0b2718e37c0437bf59d041f65fbd
Andrew Walbran <qwandor@google.com>
2023-10-25 18:19:40 +0100

    Expand values from top level config into variants when parsing.
    Often it is desirable to share configuration options across variants.
    This change allows that by taking all options at the higher level
    (top-level or package) that aren't used there and adding them to all
    variants where they are not already set. Doing so with the typed
    configuration structs would be awkward as we'd have to have a bunch of
    duplicate fields, and make all fields optional so we could tell when
    they are not set. Instead, we first parse the JSON into a dynamically
    typed representation, and then preprocess this to expand the variants
    before getting serde to parse it into the typed configuration structs.
    
    This also allows existing cargo_embargo.json configurations to work
    again, as we default to a single variant with all relevant options from
    the top level.
    
    Bug: 293289578
    Test: atest cargo_embargo.test
    Change-Id: Ib546ffc7ee5df15c4be9eb1596184d1cd67884f7

--- tools/cargo_embargo/src/config.rs
+++ tools/cargo_embargo/src/config.rs
@@ -28,8 +28,9 @@
 
 pub mod legacy;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 
@@ -54,7 +55,7 @@ fn is_false(value: &bool) -> bool {
 }
 
 /// Options that apply to everything.
-#[derive(Clone, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
     pub variants: Vec<VariantConfig>,
@@ -63,10 +64,92 @@ pub struct Config {
     pub package: BTreeMap<String, PackageConfig>,
 }
 
+/// Inserts entries from `defaults` into `variant` if neither it nor `ignored_fields` contain
+/// matching keys.
+fn add_defaults_to_variant(
+    variant: &mut Map<String, Value>,
+    defaults: &Map<String, Value>,
+    ignored_fields: &[&str],
+) {
+    for (key, value) in defaults {
+        if !ignored_fields.contains(&key.as_str()) && !variant.contains_key(key) {
+            variant.insert(key.to_owned(), value.to_owned());
+        }
+    }
+}
+
 impl Config {
+    /// Names of all fields in [`Config`] other than `variants` (which is treated specially).
+    const FIELD_NAMES: [&str; 1] = ["package"];
+
     /// Parses an instance of this config from a string of JSON.
     pub fn from_json_str(json_str: &str) -> Result<Self> {
-        serde_json::from_str(json_str).context("failed to parse config")
+        // First parse into untyped map.
+        let mut config: Map<String, Value> =
+            serde_json::from_str(json_str).context("failed to parse config")?;
+
+        // Flatten variants. First, get the variants from the config file.
+        let mut variants = match config.remove("variants") {
+            Some(Value::Array(v)) => v,
+            Some(_) => bail!("Failed to parse config: variants is not an array"),
+            None => {
+                // There are no variants, so just put everything into a single variant.
+                vec![Value::Object(Map::new())]
+            }
+        };
+        // Set default values in variants from top-level config.
+        for variant in &mut variants {
+            let variant = variant
+                .as_object_mut()
+                .context("Failed to parse config: variant is not an object")?;
+            add_defaults_to_variant(variant, &config, &Config::FIELD_NAMES);
+
+            if let Some(packages) = config.get("package") {
+                // Copy package entries across.
+                let variant_packages = variant
+                    .entry("package")
+                    .or_insert_with(|| Map::new().into())
+                    .as_object_mut()
+                    .context("Failed to parse config: variant package is not an object")?;
+                for (package_name, package_config) in packages
+                    .as_object()
+                    .context("Failed to parse config: package is not an object")?
+                {
+                    let variant_package = variant_packages
+                        .entry(package_name)
+                        .or_insert_with(|| Map::new().into())
+                        .as_object_mut()
+                        .context(
+                            "Failed to parse config: variant package config is not an object",
+                        )?;
+                    add_defaults_to_variant(
+                        variant_package,
+                        package_config
+                            .as_object()
+                            .context("Failed to parse config: package is not an object")?,
+                        &PackageConfig::FIELD_NAMES,
+                    );
+                }
+            }
+        }
+        // Remove other entries from the top-level config, and put variants back.
+        config.retain(|key, _| Self::FIELD_NAMES.contains(&key.as_str()));
+        if let Some(package) = config.get_mut("package") {
+            for value in package
+                .as_object_mut()
+                .context("Failed to parse config: package is not an object")?
+                .values_mut()
+            {
+                let package_config = value
+                    .as_object_mut()
+                    .context("Failed to parse config: package is not an object")?;
+                package_config.retain(|key, _| PackageConfig::FIELD_NAMES.contains(&key.as_str()))
+            }
+        }
+        config.insert("variants".to_string(), Value::Array(variants));
+
+        // Parse into `Config` struct.
+        serde_json::from_value(Value::Object(config)).context("failed to parse config")
     }
 
     /// Serializes an instance of this config to a string of pretty-printed JSON.
@@ -75,7 +158,7 @@ impl Config {
     }
 }
 
-#[derive(Clone, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct VariantConfig {
     /// Whether to output "rust_test" modules.
@@ -154,7 +237,7 @@ impl Default for VariantConfig {
 
 /// Options that apply to everything in a package (i.e. everything associated with a particular
 /// Cargo.toml file), for all variants.
-#[derive(Clone, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct PackageConfig {
     /// File with content to append to the end of the generated Android.bp.
@@ -165,9 +248,14 @@ pub struct PackageConfig {
     pub patch: Option<PathBuf>,
 }
 
+impl PackageConfig {
+    /// Names of all the fields on `PackageConfig`.
+    const FIELD_NAMES: [&str; 2] = ["add_toplevel_block", "patch"];
+}
+
 /// Options that apply to everything in a package (i.e. everything associated with a particular
 /// Cargo.toml file), for a particular variant.
-#[derive(Clone, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct PackageVariantConfig {
     /// Link against `alloc`. Only valid if `no_std` is also true.
@@ -225,5 +313,119 @@ impl Default for PackageVariantConfig {
             copy_out: false,
             test_data: Default::default(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn variant_config() {
+        let config = Config::from_json_str(
+            r#"{
+            "tests": true,
+            "package": {
+                "argh": {
+                    "patch": "patches/Android.bp.patch"
+                },
+                "another": {
+                    "add_toplevel_block": "block.bp",
+                    "device_supported": false,
+                    "force_rlib": true
+                }
+            },
+            "variants": [
+                {},
+                {
+                    "tests": false,
+                    "features": ["feature"],
+                    "vendor_available": false,
+                    "package": {
+                        "another": {
+                            "alloc": false,
+                            "force_rlib": false
+                        },
+                        "variant_package": {
+                            "add_module_block": "variant_module_block.bp"
+                        }
+                    }
+                }
+            ]
+        }"#,
+        )
+        .unwrap();
+
+        assert_eq!(
+            config,
+            Config {
+                variants: vec![
+                    VariantConfig {
+                        tests: true,
+                        features: None,
+                        vendor_available: true,
+                        package: [
+                            ("argh".to_string(), PackageVariantConfig { ..Default::default() }),
+                            (
+                                "another".to_string(),
+                                PackageVariantConfig {
+                                    device_supported: false,
+                                    force_rlib: true,
+                                    ..Default::default()
+                                },
+                            ),
+                        ]
+                        .into_iter()
+                        .collect(),
+                        ..Default::default()
+                    },
+                    VariantConfig {
+                        tests: false,
+                        features: Some(vec!["feature".to_string()]),
+                        vendor_available: false,
+                        package: [
+                            ("argh".to_string(), PackageVariantConfig { ..Default::default() }),
+                            (
+                                "another".to_string(),
+                                PackageVariantConfig {
+                                    alloc: false,
+                                    device_supported: false,
+                                    force_rlib: false,
+                                    ..Default::default()
+                                },
+                            ),
+                            (
+                                "variant_package".to_string(),
+                                PackageVariantConfig {
+                                    add_module_block: Some("variant_module_block.bp".into()),
+                                    ..Default::default()
+                                },
+                            ),
+                        ]
+                        .into_iter()
+                        .collect(),
+                        ..Default::default()
+                    },
+                ],
+                package: [
+                    (
+                        "argh".to_string(),
+                        PackageConfig {
+                            patch: Some("patches/Android.bp.patch".into()),
+                            ..Default::default()
+                        },
+                    ),
+                    (
+                        "another".to_string(),
+                        PackageConfig {
+                            add_toplevel_block: Some("block.bp".into()),
+                            ..Default::default()
+                        },
+                    ),
+                ]
+                .into_iter()
+                .collect(),
+            }
+        );
     }
 }
