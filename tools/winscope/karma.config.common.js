7bb9a0ce2c117ac75768c57cb66c0049ded7c218
Nataniel Borges <natanieljr@google.com>
2024-05-22 09:47:21 +0000

    Use different coverage reporter to fix istanbul issues with source mapping
    Needs a new `npm install` before running the tests
    
    Example: go/winscope-coverage
    Test: npm run tes:unit:ci
    Bug: 206978580
    Change-Id: I34b559a2e07b10022ac9460be7496742ffb463ab

--- tools/winscope/karma.config.common.js
+++ tools/winscope/karma.config.common.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+const path = require('path');
 const webpackConfig = require('./webpack.config.common');
 delete webpackConfig.entry;
 delete webpackConfig.output;
@@ -22,7 +23,7 @@ module.exports = (config) => {
     frameworks: ['jasmine', 'webpack'],
     plugins: [
       'karma-webpack',
-      'karma-coverage',
+      'karma-coverage-istanbul-reporter',
       'karma-chrome-launcher',
       'karma-jasmine',
       'karma-sourcemap-loader',
@@ -41,19 +42,54 @@ module.exports = (config) => {
         served: true,
       },
     ],
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage-istanbul'],
     preprocessors: {
       'src/main_unit_test.ts': ['webpack', 'sourcemap'],
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
-      'src/**/!(*test).ts': ['coverage'],
     },
-    // optionally, configure the reporter
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/',
+    coverageIstanbulReporter: {
+      // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/73c25ce79f91010d1ff073aa6ff3fd01114f90db/packages/istanbul-reports/lib
+      reports: ['html', 'lcovonly', 'text-summary'],
+
+      // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      dir: path.join(__dirname, 'coverage'),
+
+      // if using webpack and pre-loaders, work around webpack breaking the source path
+      fixWebpackSourcePaths: true,
+
+      // Omit files with no statements, no functions and no branches covered from the report
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these through the `report-config` option
+      'report-config': {
+        // all options available at: https://github.com/istanbuljs/istanbuljs/blob/73c25ce79f91010d1ff073aa6ff3fd01114f90db/packages/istanbul-reports/lib/html/index.js#L257-L261
+        html: {
+          // outputs the report in ./coverage/html
+          subdir: 'html',
+        },
+      },
+
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        },
+        // thresholds per file
+        each: {
+          statements: 50,
+          lines: 50,
+          branches: 50,
+          functions: 50,
+        },
+      },
     },
+
+    verbose: true, // output config used by istanbul for debugging
     webpack: webpackConfig,
   });
 };
