befa102463f31ef54a79891493d2260d759de442
Priyanka <priyankaspatel@google.com>
2024-08-30 17:45:57 +0100

    Filter flags and search box component.
    Bug: 360306350
    Test: npm run test:unit:ci
    Change-Id: I4b0e0e65acb399b114424198e20fa0e68f40c221

---
+++ tools/winscope/src/viewers/components/search_box_component_test.ts
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {assertDefined} from 'common/assert_utils';
+import {FilterFlag} from 'common/filter_flag';
+import {InMemoryStorage} from 'common/in_memory_storage';
+import {TextFilterDetail} from 'viewers/common/viewer_events';
+import {SearchBoxComponent} from './search_box_component';
+
+describe('SearchBoxComponent', () => {
+  let fixture: ComponentFixture<SearchBoxComponent>;
+  let component: SearchBoxComponent;
+  let htmlElement: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatTooltipModule,
+      ],
+      declarations: [SearchBoxComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(SearchBoxComponent);
+    component = fixture.componentInstance;
+    htmlElement = fixture.nativeElement;
+    component.store = new InMemoryStorage();
+    component.storeKey = 'testStoreKey.searchBox';
+    fixture.detectChanges();
+  });
+
+  it('can be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('shows custom label', () => {
+    const label = htmlElement.querySelector('.search-box mat-label');
+    expect(label?.textContent).toEqual('Search');
+
+    component.label = 'custom label';
+    fixture.detectChanges();
+    expect(label?.textContent).toEqual('custom label');
+  });
+
+  it('handles change in filter', () => {
+    const spy = spyOn(component.filterChange, 'emit');
+    expect(component.filterString).toBe('');
+    changeFilterString('Test');
+    expect(component.filterString).toBe('Test');
+    expect(spy).toHaveBeenCalledWith(new TextFilterDetail('Test', []));
+  });
+
+  it('handles change in flags', () => {
+    const spy = spyOn(component.filterChange, 'emit');
+    const buttons =
+      htmlElement.querySelectorAll<HTMLElement>('.search-box button');
+    expect(buttons.length).toEqual(3);
+
+    buttons.item(0).click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(
+      new TextFilterDetail('', [FilterFlag.MATCH_CASE]),
+    );
+
+    buttons.item(0).click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(new TextFilterDetail('', []));
+
+    buttons.item(2).click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(
+      new TextFilterDetail('', [FilterFlag.USE_REGEX]),
+    );
+
+    buttons.item(1).click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(
+      new TextFilterDetail('', [FilterFlag.USE_REGEX, FilterFlag.MATCH_WORD]),
+    );
+  });
+
+  it('stores flag changes', () => {
+    const button = assertDefined(
+      htmlElement.querySelector<HTMLElement>('.search-box button'),
+    );
+    button.click();
+    fixture.detectChanges();
+
+    const newFixture = TestBed.createComponent(SearchBoxComponent);
+    const newComponent = newFixture.componentInstance;
+    const newHtmlElement = newFixture.nativeElement;
+    newComponent.store = component.store;
+    newComponent.storeKey = component.storeKey;
+    newFixture.detectChanges();
+
+    const spy = spyOn(newComponent.filterChange, 'emit');
+    changeFilterString('Test', newHtmlElement, newFixture);
+    expect(spy).toHaveBeenCalledWith(
+      new TextFilterDetail('Test', [FilterFlag.MATCH_CASE]),
+    );
+  });
+
+  function changeFilterString(
+    newString: string,
+    el = htmlElement,
+    f = fixture,
+  ) {
+    const inputEl = assertDefined(
+      el.querySelector<HTMLInputElement>('.search-box input'),
+    );
+    inputEl.value = newString;
+    inputEl.dispatchEvent(new Event('input'));
+    f.detectChanges();
+  }
+});
