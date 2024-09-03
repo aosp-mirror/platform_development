dd8e95f30e613c6dd2ea0d56f92358c9bc0ec4e3
Per Larsen <perlarsen@google.com>
2024-09-03 21:33:22 +0000

    cargo_embargo: add BSD-2-Clause OR Apache-2 OR MIT
    Add BSD-2-Clause OR Apache-2.0 OR MIT license matching and select
    Apache-2.0
    
    Test: manually, run `cargo_embargo generate cargo_embargo.json` for
          zerocopy and check that Android.bp is valid with `m nothing`.
    Change-Id: I9a60527920d84c8b82641017f6521cfcd3cbf745

--- tools/cargo_embargo/src/main.rs
+++ tools/cargo_embargo/src/main.rs
@@ -753,6 +753,7 @@ fn choose_license(license: &str) -> &str {
         "Apache-2.0 / MIT" => "Apache-2.0",
         "Unlicense OR MIT" => "MIT",
         "BSD-3-Clause OR MIT OR Apache-2.0" => "Apache-2.0",
+        "BSD-2-Clause OR Apache-2.0 OR MIT" => "Apache-2.0",
         _ => license,
     }
 }
