e0c95b0bfc7f17be8e2dec4669d8c94838818f5e
Priyanka <priyankaspatel@google.com>
2025-01-17 21:09:19 +0000

    Dedupe tooltip checks in tests.
    Bug: n/a
    Test: npm run test:unit:ci
    Change-Id: I7389ec454e946c162b91b077f0d4499552d126d6

--- tools/winscope/src/viewers/viewer_search/search_list_component_test.ts
+++ tools/winscope/src/viewers/viewer_search/search_list_component_test.ts
@@ -23,6 +23,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {assertDefined} from 'common/assert_utils';
+import {UnitTestUtils} from 'test/unit/utils';
 import {ListItemOption, SearchListComponent} from './search_list_component';
 import {ListedSearch} from './ui_data';
 
@@ -60,7 +61,7 @@ describe('SearchListComponent', () => {
     expect(htmlElement.textContent?.trim()).toEqual(placeholderText);
   });
 
-  it('shows search names with tooltips', () => {
+  it('shows search names with tooltips', async () => {
     component.searches = [
       new ListedSearch('query1', 'name1'),
       new ListedSearch('query2', 'query2'),
@@ -81,19 +82,15 @@ describe('SearchListComponent', () => {
     expect(queryName2.textContent?.trim()).toEqual('query2');
 
     // shows tooltip when name and query are different
-    checkTooltip(queryName1, 'name1: query1');
+    await UnitTestUtils.checkTooltips([queryName1], ['name1: query1'], fixture);
 
     // does not show tooltip when name and query are the same
-    queryName2.dispatchEvent(new Event('mouseenter'));
-    fixture.detectChanges();
-    expect(
-      document.querySelector<HTMLElement>('.mat-tooltip-panel'),
-    ).toBeNull();
+    await UnitTestUtils.checkTooltips([queryName2], [undefined], fixture);
 
     // shows tooltip when element is overflowing
     queryName2.style.maxWidth = queryName2.offsetWidth / 2 + 'px';
     fixture.detectChanges();
-    checkTooltip(queryName2, 'query2');
+    await UnitTestUtils.checkTooltips([queryName2], ['query2'], fixture);
   });
 
   it('formats search dates', () => {
@@ -128,7 +125,7 @@ describe('SearchListComponent', () => {
     const option = assertDefined(
       htmlElement.querySelector<HTMLElement>('.listed-search-option'),
     );
-    checkTooltip(option, 'option1');
+    UnitTestUtils.checkTooltips([option], ['option1'], fixture);
     option.click();
     expect(optionClicked).toEqual(component.searches[0]);
   });
@@ -142,25 +139,13 @@ describe('SearchListComponent', () => {
     const option = assertDefined(
       htmlElement.querySelector<HTMLElement>('.listed-search-option'),
     );
-    checkTooltip(option, 'option1');
+    UnitTestUtils.checkTooltips([option], ['option1'], fixture);
     option.click();
     const menu = assertDefined(
       document.querySelector<HTMLElement>('.context-menu'),
     );
     expect(menu.querySelector('.test-menu-item')).toBeTruthy();
   });
-
-  function checkTooltip(element: HTMLElement, tooltip: string) {
-    element.dispatchEvent(new Event('mouseenter'));
-    fixture.detectChanges();
-    expect(
-      assertDefined(
-        document.querySelector<HTMLElement>('.mat-tooltip-panel'),
-      ).textContent?.trim(),
-    ).toEqual(tooltip);
-    element.dispatchEvent(new Event('mouseleave'));
-    fixture.detectChanges();
-  }
 
   @Component({
     selector: 'host-component',
