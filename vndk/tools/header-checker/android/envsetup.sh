06b8ca938f9e7a3907835ba1b9a0d614a5d5f15d
Ryan Prichard <rprichard@google.com>
2025-02-11 15:57:34 -0800

    Update to clang-r547379
    Pass true for the ElideOverrideInfo parameters to disable the new
    text added to mangled identifiers, to preserve the existing mangling
    behavior. See https://llvm.org/pr94056.
    
    Test: presubmit
    Bug: http://b/379133546
    Change-Id: I26cce1f391c3b83240187abd403ab23ace90696c

--- vndk/tools/header-checker/android/envsetup.sh
+++ vndk/tools/header-checker/android/envsetup.sh
@@ -15,5 +15,5 @@
 # limitations under the License.
 
 export LLVM_BUILD_HOST_TOOLS=true
-export LLVM_PREBUILTS_VERSION=clang-r536225
-export LLVM_RELEASE_VERSION=19
+export LLVM_PREBUILTS_VERSION=clang-r547379
+export LLVM_RELEASE_VERSION=20
